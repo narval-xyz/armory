22801249e7e3de1e482efb8ea4a793e4f5fd0cb2
William Calderipe <wcalderipe@gmail.com>
2024-01-03 15:57:49 +0100

    Add example of Orchestration pinging a AuthZ cluster

--- apps/orchestration/src/orchestration.module.ts
+++ apps/orchestration/src/orchestration.module.ts
@@ -1,10 +1,9 @@
 import { TransactionEngineModule } from '@narval/transaction-engine-module'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { HealthCheckModule } from './health-check/health-check.module'
 import { PolicyEngineModule } from './policy-engine/policy-engine.module'
 
 @Module({
-  imports: [ConfigModule.forRoot(), PolicyEngineModule, TransactionEngineModule, HealthCheckModule]
+  imports: [ConfigModule.forRoot(), PolicyEngineModule, TransactionEngineModule]
 })
 export class OrchestrationModule {}
