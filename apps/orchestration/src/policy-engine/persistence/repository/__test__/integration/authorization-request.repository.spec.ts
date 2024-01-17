731243f7352baeb7fae092e8117a0a4c1aace4bd
William Calderipe <wcalderipe@gmail.com>
2024-01-17 19:10:49 +0100

    Fix @app/orchestration unit and integration tests

--- apps/orchestration/src/policy-engine/persistence/repository/__test__/integration/authorization-request.repository.spec.ts
+++ apps/orchestration/src/policy-engine/persistence/repository/__test__/integration/authorization-request.repository.spec.ts
@@ -124,9 +124,9 @@ describe(AuthorizationRequestRepository.name, () => {
         action: Action.SIGN_TRANSACTION,
         request: {
           from: '0xaaa8ee1cbaa1856f4550c6fc24abb16c5c9b2a43',
-          to: '0xbbb7be636c3ad8cf9d08ba8bdba4abd2ef29bd23',
-          data: '0x',
-          gas: BigInt(5_000)
+          gas: BigInt(5_000),
+          chainId: 1,
+          nonce: 1
         }
       }
 
