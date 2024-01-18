5b822b6390af8373bb9a0454e1462c9ab303d36f
Matt Schoch <matt@narval.xyz>
2024-01-17 21:01:43 -0500

    Adding transfers into the EvaluationRequestDto

--- apps/authz/src/shared/types/rego.ts
+++ apps/authz/src/shared/types/rego.ts
@@ -1,23 +1,6 @@
-import { Caip10 } from 'packages/transaction-request-intent/src/lib/caip'
 import { Intent } from 'packages/transaction-request-intent/src/lib/intent.types'
 import { Action } from './enums'
-import { ApprovalRequirement, AuthCredential, TransactionRequest } from './http'
-
-export enum FiatSymbols {
-  USD = 'fiat:usd',
-  EUR = 'fiat:eur'
-}
-
-export type HistoricalTransfer = {
-  amount: string // Amount in the smallest unit of the token (eg. wei for ETH)
-  from: Caip10
-  to: Caip10 // In case we want spending limit per destination address
-  chainId: number
-  token: Caip10
-  rates: { [keyof in FiatSymbols]: string } // eg. { fiat:usd: '0.01', fiat:eur: '0.02' }
-  initiatedBy: string // uid of the user who initiated the spending
-  timestamp: number // unix timestamp
-}
+import { ApprovalRequirement, AuthCredential, HistoricalTransfer, TransactionRequest } from './http'
 
 export type RegoInput = {
   action: Action
