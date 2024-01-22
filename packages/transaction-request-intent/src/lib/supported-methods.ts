8920c5ca6f0296109e12d2887c7515885129e0ef
William Calderipe <wcalderipe@gmail.com>
2024-01-22 14:45:47 +0100

    Halt trunk-based development (#27)
    * Send CI Slack notification only on main branch
    
    * Add test:type recipe and CI step
    
    * Fix type errors from @narval/transaction-request-intent
    
    Co-authored-by: Pierre Troger <pierre@narval.xyz>

--- packages/transaction-request-intent/src/lib/supported-methods.ts
+++ packages/transaction-request-intent/src/lib/supported-methods.ts
@@ -1,6 +1,7 @@
 import { AbiParameter } from 'viem'
 import { AssetTypeEnum, Intents } from './domain'
 import {
+  ApproveAllowanceParamsTransform,
   Erc1155SafeTransferFromParamsTransform,
   Erc721SafeTransferFromParamsTransform,
   TransferBatchTransferParamsTransform,
@@ -8,6 +9,7 @@ import {
   TransferParamsTransform
 } from './extraction/transformers'
 import {
+  ApproveAllowanceParams,
   Erc1155SafeTransferFromParams,
   Erc721SafeTransferFromParams,
   SafeBatchTransferFromParams,
@@ -80,9 +82,20 @@ export const Erc1155SafeBatchTransferFromAbiParameters: AbiParameter[] = [
   }
 ]
 
+export const Erc20TransferAbiParameters: AbiParameter[] = [
+  { type: 'address', name: 'recipient' },
+  { type: 'uint256', name: 'amount' }
+]
+
+export const ApproveAllowanceAbiParameters: AbiParameter[] = [
+  { type: 'address', name: 'spender' },
+  { type: 'uint256', name: 'amount' }
+]
+
 export enum SupportedMethodId {
   TRANSFER = '0xa9059cbb',
   TRANSFER_FROM = '0x23b872dd',
+  APPROVE = '0xa613914d',
   SAFE_TRANSFER_FROM = '0x42842e0e',
   SAFE_TRANSFER_FROM_WITH_BYTES = '0xb88d4fde',
   SAFE_BATCH_TRANSFER_FROM = '0x2eb2c2d6',
@@ -91,16 +104,12 @@ export enum SupportedMethodId {
   NULL_METHOD_ID = '0x00000000'
 }
 
-export const Erc20TransferAbiParameters: AbiParameter[] = [
-  { type: 'address', name: 'recipient' },
-  { type: 'uint256', name: 'amount' }
-]
-
 // !! this is not taking into account the 'bytes' parameter.
 // To access this parameter, change the expected params and the data transformer associated to it.
 export type StandardMethodsParams = {
   [SupportedMethodId.TRANSFER]: TransferParams
   [SupportedMethodId.TRANSFER_FROM]: TransferFromParams
+  [SupportedMethodId.APPROVE]: ApproveAllowanceParams
   [SupportedMethodId.SAFE_TRANSFER_FROM]: Erc721SafeTransferFromParams
   [SupportedMethodId.SAFE_TRANSFER_FROM_WITH_BYTES]: Erc721SafeTransferFromParams
   [SupportedMethodId.SAFE_BATCH_TRANSFER_FROM]: SafeBatchTransferFromParams
@@ -167,6 +176,12 @@ export const SUPPORTED_METHODS: MethodsMapping = {
     transformer: Erc1155SafeTransferFromParamsTransform,
     assetType: AssetTypeEnum.ERC1155,
     intent: Intents.TRANSFER_ERC1155
+  },
+  [SupportedMethodId.APPROVE]: {
+    name: 'approve',
+    abi: ApproveAllowanceAbiParameters,
+    transformer: ApproveAllowanceParamsTransform,
+    assetType: AssetTypeEnum.UNKNOWN
   },
   [SupportedMethodId.NULL_METHOD_ID]: {
     name: 'nativeTransfer',
