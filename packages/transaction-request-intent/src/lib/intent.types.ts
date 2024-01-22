f3f65f66db99bdc1f2a21f5d6d512abc565604b2
Pierre Troger <pierre@narval.xyz>
2024-01-22 09:29:43 -0400

    format

--- packages/transaction-request-intent/src/lib/intent.types.ts
+++ packages/transaction-request-intent/src/lib/intent.types.ts
@@ -125,4 +125,10 @@ export type Permit2 = {
   deadline: string
 }
 
-export type Intent = TransferNative | TransferErc20 | TransferErc721 | TransferErc1155 | CallContract | ApproveTokenAllowance
+export type Intent =
+  | TransferNative
+  | TransferErc20
+  | TransferErc721
+  | TransferErc1155
+  | CallContract
+  | ApproveTokenAllowance
