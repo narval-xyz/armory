83189c7aafe7a25290542f4e65c3e7e829993ed6
William Calderipe <wcalderipe@gmail.com>
2024-01-26 16:55:57 +0100

    Remove Jest moduleNameMapper (#61)

--- packages/authz-shared/jest.config.ts
+++ packages/authz-shared/jest.config.ts
@@ -2,14 +2,16 @@ import type { Config } from 'jest'
 
 const config: Config = {
   displayName: 'authz-shared',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  moduleNameMapper: {
-    '^@narval/authz-shared/(.*)$': '<rootDir>/src/$1'
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json'
+      }
+    ]
   }
 }
 
