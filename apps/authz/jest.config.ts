be0d7c0d79b6a9176e461874600a2f5f2a9324cb
William Calderipe <wcalderipe@gmail.com>
2024-01-26 16:55:57 +0100

    Remove Jest moduleNameMapper (#61)

--- apps/authz/jest.config.ts
+++ apps/authz/jest.config.ts
@@ -2,15 +2,16 @@ import type { Config } from 'jest'
 
 const config: Config = {
   displayName: 'authz',
+  moduleFileExtensions: ['ts', 'js', 'html'],
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/authz',
-  moduleNameMapper: {
-    '^@app/authz/(.*)$': '<rootDir>/src/$1'
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json'
+      }
+    ]
   }
 }
 
--- packages/transaction-request-intent/jest.config.ts
+++ packages/transaction-request-intent/jest.config.ts
@@ -1,14 +1,10 @@
 import type { Config } from 'jest'
 
 const config: Config = {
-  coverageDirectory: '../../coverage/packages/transaction-request-intent',
-  testEnvironment: 'node',
   displayName: 'transaction-request-intent',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  moduleNameMapper: {
-    '^@narval/transaction-request-intent/(.*)$': '<rootDir>/src/$1'
-  },
   preset: '../../jest.preset.js',
+  testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]sx?$': [
       'ts-jest',
