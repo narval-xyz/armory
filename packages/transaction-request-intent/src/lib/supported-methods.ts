f0bfe66d02ed2cd7784c468f74d8dce8ab9f5957
Ptroger <44851272+Ptroger@users.noreply.github.com>
2024-01-29 14:39:47 -0400

    Support general typed data (#66)
    * TypedData supported and tested
    
    * fixed type error

--- packages/transaction-request-intent/src/lib/supported-methods.ts
+++ packages/transaction-request-intent/src/lib/supported-methods.ts
@@ -315,3 +315,41 @@ export const SUPPORTED_METHODS: MethodsMapping = {
     intent: Intents.TRANSFER_NATIVE
   }
 }
+
+export const permitTypedData = {
+  types: {
+    EIP712Domain: [
+      { name: 'name', type: 'string' },
+      { name: 'version', type: 'string' },
+      { name: 'chainId', type: 'uint256' },
+      { name: 'verifyingContract', type: 'address' }
+    ],
+    Permit: [
+      { name: 'owner', type: 'address' },
+      { name: 'spender', type: 'address' },
+      { name: 'value', type: 'uint256' },
+      { name: 'nonce', type: 'uint256' },
+      { name: 'deadline', type: 'uint256' }
+    ]
+  },
+  primaryType: 'Permit' as const
+}
+
+export const permit2TypedData = {
+  types: {
+    EIP712Domain: [
+      { name: 'name', type: 'string' },
+      { name: 'version', type: 'string' },
+      { name: 'chainId', type: 'uint256' },
+      { name: 'verifyingContract', type: 'address' }
+    ],
+    Permit: [
+      { name: 'owner', type: 'address' },
+      { name: 'spender', type: 'address' },
+      { name: 'value', type: 'uint256' },
+      { name: 'nonce', type: 'uint256' },
+      { name: 'deadline', type: 'uint256' }
+    ]
+  },
+  primaryType: 'Permit' as const
+}
