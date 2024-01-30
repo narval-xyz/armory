3ae29c2375f932ba2c6eb23e7c4a509f21720690
William Calderipe <wcalderipe@gmail.com>
2024-01-30 14:43:55 +0100

    Format CoinGecko coin index JSON

--- apps/orchestration/src/price/http/client/coin-gecko/__test__/fixture/coin-gecko.fixture.ts
+++ apps/orchestration/src/price/http/client/coin-gecko/__test__/fixture/coin-gecko.fixture.ts
@@ -7,10 +7,6 @@ export const generateSimplePrice = (): SimplePrice => {
       usd_market_cap: 272895913402.66522,
       usd_24h_vol: 6827306177.886258,
       usd_24h_change: -0.8104607737461819,
-      eur: 2101.0645108266203,
-      eur_market_cap: 252292271940.76367,
-      eur_24h_vol: 6313107613.0987625,
-      eur_24h_change: -0.36025898833091113,
       last_updated_at: 1706524566
     },
     uniswap: {
@@ -18,10 +14,6 @@ export const generateSimplePrice = (): SimplePrice => {
       usd_market_cap: 4496856084.101798,
       usd_24h_vol: 74037282.24412505,
       usd_24h_change: -0.8629967474909083,
-      eur: 5.522934362768995,
-      eur_market_cap: 4157343449.752105,
-      eur_24h_vol: 68447467.43469352,
-      eur_24h_change: -0.4329575908228408,
       last_updated_at: 1706524541
     }
   }
