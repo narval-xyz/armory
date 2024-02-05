7db62d1611df80b7d278acd27fd1a0b6d780176f
Matt Schoch <matt@narval.xyz>
2024-02-05 15:55:38 -0500

    POST /address-book

---
+++ apps/authz/src/app/http/rest/dto/address-book-account-dto.ts
@@ -0,0 +1,35 @@
+import { AddressBookAccount } from '@app/authz/shared/types/entities.types'
+import { AccountClassification, Address } from '@narval/authz-shared'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsDefined, IsIn, IsNumber, IsString } from 'class-validator'
+
+export class AddressBookAccountDataDto {
+  constructor(addressBookAccount: AddressBookAccount) {
+    this.uid = addressBookAccount.uid
+    this.classification = addressBookAccount.classification
+    this.address = addressBookAccount.address
+    this.chainId = addressBookAccount.chainId
+  }
+
+  @IsString()
+  @IsDefined()
+  @ApiProperty()
+  uid: string
+
+  @IsIn(Object.values(AccountClassification))
+  @IsDefined()
+  @ApiProperty({
+    enum: Object.values(AccountClassification)
+  })
+  classification: AccountClassification
+
+  @IsString()
+  @IsDefined()
+  @ApiProperty()
+  address: Address
+
+  @IsNumber()
+  @IsDefined()
+  @ApiProperty()
+  chainId: number
+}
