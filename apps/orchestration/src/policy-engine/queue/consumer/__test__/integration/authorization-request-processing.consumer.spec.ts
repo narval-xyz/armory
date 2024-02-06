f9456eb7b486452f8a7eaed321ee153567396e3c
William Calderipe <wcalderipe@gmail.com>
2024-02-06 11:20:05 +0100

    Add test coverage for job consuming retry logic

--- apps/orchestration/src/policy-engine/queue/consumer/__test__/integration/authorization-request-processing.consumer.spec.ts
+++ apps/orchestration/src/policy-engine/queue/consumer/__test__/integration/authorization-request-processing.consumer.spec.ts
@@ -4,8 +4,13 @@ import {
   AUTHORIZATION_REQUEST_PROCESSING_QUEUE,
   AUTHORIZATION_REQUEST_PROCESSING_QUEUE_ATTEMPTS
 } from '@app/orchestration/orchestration.constant'
+import { ClusterNotFoundException } from '@app/orchestration/policy-engine/core/exception/cluster-not-found.exception'
+import { EvaluationConsensusException } from '@app/orchestration/policy-engine/core/exception/evaluation-consensus.exception'
+import { InvalidAttestationSignatureException } from '@app/orchestration/policy-engine/core/exception/invalid-attestation-signature.exception'
+import { UnreachableClusterException } from '@app/orchestration/policy-engine/core/exception/unreachable-cluster.exception'
 import { AuthorizationRequestService } from '@app/orchestration/policy-engine/core/service/authorization-request.service'
 import { ClusterService } from '@app/orchestration/policy-engine/core/service/cluster.service'
+import { Cluster } from '@app/orchestration/policy-engine/core/type/clustering.type'
 import {
   AuthorizationRequest,
   AuthorizationRequestProcessingJob,
@@ -131,6 +136,13 @@ describe(AuthorizationRequestProcessingConsumer.name, () => {
   })
 
   describe('process', () => {
+    const job = mock<Job<AuthorizationRequestProcessingJob>>({
+      id: authzRequest.id,
+      opts: {
+        jobId: authzRequest.id
+      }
+    })
+
     it('calls the service to process the authorization request job', async () => {
       // Prevent calling the node at the evaluation phase.
       jest.spyOn(service, 'evaluate').mockResolvedValue({
@@ -141,16 +153,39 @@ describe(AuthorizationRequestProcessingConsumer.name, () => {
 
       jest.spyOn(service, 'process')
 
-      const job = mock<Job<AuthorizationRequestProcessingJob>>({
-        id: authzRequest.id,
-        opts: {
-          jobId: authzRequest.id
-        }
-      })
-
       await consumer.process(job)
 
       expect(service.process).toHaveBeenCalledWith(authzRequest.id)
+    })
+
+    // Before you start with these tests, learn how Bull works. Jobs in Bull can
+    // retry, but we don't want retries for certain errors. To stop a job from
+    // retrying, the process method needs to return an error instance. In
+    // contrast, to let the retry happen, the process method must throw.
+    describe('when job fails', () => {
+      it('retries on unknown error', async () => {
+        const error = new Error('unknown error')
+        jest.spyOn(service, 'process').mockRejectedValue(error)
+
+        expect(consumer.process(job)).rejects.toThrow(error)
+      })
+
+      it('stops retrying on known unrecoverable errors', async () => {
+        const unrecoverableErrors = [
+          new ClusterNotFoundException(authzRequest.orgId),
+          new EvaluationConsensusException([], []),
+          new UnreachableClusterException(mock<Cluster>()),
+          new InvalidAttestationSignatureException('test-pubkey', 'test-recovered-pubkey')
+        ]
+
+        expect.assertions(unrecoverableErrors.length)
+
+        unrecoverableErrors.forEach(async (error) => {
+          jest.spyOn(service, 'process').mockRejectedValue(error)
+
+          expect(await consumer.process(job)).toEqual(error)
+        })
+      })
     })
   })
 
