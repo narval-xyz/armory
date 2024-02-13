121d3b78782394cc15da4d53962466dfe2e59681
Samuel <samuel@narval.xyz>
2024-02-13 21:19:17 +0100

    fix (#109)

--- packages/authz-shared/src/lib/decorators/is-not-empty-array-enum.decorator.ts
+++ packages/authz-shared/src/lib/decorators/is-not-empty-array-enum.decorator.ts
@@ -1,5 +1,5 @@
 import { applyDecorators } from '@nestjs/common'
-import { ArrayNotEmpty, IsEnum, IsNotEmpty } from 'class-validator'
+import { ArrayNotEmpty, IsEnum } from 'class-validator'
 
 export function IsNotEmptyArrayEnum(obj: object) {
   return applyDecorators(ArrayNotEmpty(), IsEnum(obj, { each: true }))
