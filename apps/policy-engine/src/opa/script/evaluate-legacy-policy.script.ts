d92e47ff62e09760b192787b26ac278ddb580df0
samuel <samuel@narval.xyz>
2024-02-26 13:14:30 +0100

    translate legacy requests wip

---
+++ apps/policy-engine/src/opa/script/evaluate-legacy-policy.script.ts
@@ -0,0 +1,99 @@
+import { Action, Request, UserRole } from '@narval/policy-engine-shared'
+import { InputType, safeDecode } from '@narval/transaction-request-intent'
+import { loadPolicy } from '@open-policy-agent/opa-wasm'
+import { readFileSync } from 'fs'
+import path from 'path'
+import { OpaResult, RegoInput } from '../../shared/types/domain.type'
+import users from './data/users.json'
+import wallets from './data/wallets.json'
+import legacyRequests from './requests/legacy-requests.json'
+
+export const run = async () => {
+  const entities: { [key: string]: any } = { users: {}, wallets: {} }
+
+  for (const user of users) {
+    let role = user.guildUserRole
+    if (['SCHOLAR'].includes(role)) {
+      role = UserRole.MEMBER
+    } else if (['ADMIN', 'API'].includes(role)) {
+      role = UserRole.ADMIN
+    }
+    entities.users[user.id] = { uid: user.id, role }
+  }
+
+  for (const wallet of wallets) {
+    const uid = `eip155:${wallet.accountType}:${wallet.address}`
+    entities.wallets[uid] = {
+      uid,
+      address: wallet.address,
+      accountType: wallet.accountType,
+      assignees: wallet.assignees?.map((assignee) => assignee.userId) || []
+    }
+  }
+
+  const requests = legacyRequests.filter(
+    ({ initiator_user_id }) => undefined !== entities.users[initiator_user_id]
+  ) as {
+    status: string
+    initiator_user_id: string
+    request: Request
+  }[]
+
+  for (const { status, initiator_user_id, request } of requests) {
+    let input = {} as RegoInput
+
+    if (request.action === Action.SIGN_TRANSACTION) {
+      const intentResult = safeDecode({
+        input: {
+          type: InputType.TRANSACTION_REQUEST,
+          txRequest: request.transactionRequest
+        }
+      })
+
+      if (intentResult?.success === false) {
+        console.log(
+          `Could not decode intent: ${intentResult.error.message}`,
+          JSON.stringify(request.transactionRequest, null, 2)
+        )
+        continue
+      }
+
+      input = {
+        action: request.action,
+        intent: intentResult?.intent,
+        transactionRequest: request.transactionRequest,
+        principal: {
+          uid: initiator_user_id,
+          userId: initiator_user_id,
+          alg: 'ES256K',
+          pubKey: ''
+        },
+        resource: request.resourceId ? { uid: request.resourceId } : undefined,
+        approvals: []
+      }
+    }
+
+    const OPA_WASM_PATH = path.join(process.cwd(), './rego-build/policy.wasm')
+    const policyWasm = readFileSync(OPA_WASM_PATH)
+    const opaEngine = await loadPolicy(policyWasm, undefined, { 'time.now_ns': () => new Date().getTime() * 1000000 })
+    opaEngine.setData({ entities })
+
+    const evalResult: { result: OpaResult }[] = await opaEngine.evaluate(input, 'main/evaluate')
+    const results = evalResult.map(({ result }) => result)
+
+    // if (request.action === Action.SIGN_TRANSACTION && status == 'denied' && results[0].permit) {
+    //   // console.log({ id: results[0].reasons.map((reason) => reason.policyName), status, result: results[0].permit })
+    //   if ((input.intent as any).type.includes('transfer')) {
+    //     console.log({ intent: input.intent, initiator_user_id, status, result: results[0].permit })
+    //   }
+    // }
+
+    if (request.action === Action.SIGN_TRANSACTION && status == 'completed' && !results[0].permit) {
+      console.log({ id: results[0].reasons.map((reason) => reason.policyName), status, result: results[0].permit })
+    }
+  }
+}
+
+run()
+  .then(() => console.log('done'))
+  .catch((error) => console.log('error', error))
--- apps/policy-engine/src/opa/script/translate-legacy-policy.script.ts
+++ apps/policy-engine/src/opa/script/translate-legacy-policy.script.ts
@@ -1,5 +1,6 @@
 import { EntityType, FiatCurrency, UserRole, ValueOperators } from '@narval/policy-engine-shared'
 import axios from 'axios'
+import { omit } from 'lodash'
 import { Address, Hex } from 'viem'
 import {
   ApprovalCondition,
@@ -9,12 +10,13 @@ import {
   SignTypedDataDomainCondition,
   Then
 } from '../../shared/types/policy.type'
+import data from './policies/policy_rules_ngg_prod.json'
 
 type OldPolicy = { [key: string]: string | null }
 
 type NewPolicy = Policy & { id: string }
 
-const translateLegacyPolicy = (oldPolicy: OldPolicy): NewPolicy | null => {
+export const translateLegacyPolicy = (oldPolicy: OldPolicy): NewPolicy | null => {
   const {
     id,
     result,
@@ -47,16 +49,12 @@ const translateLegacyPolicy = (oldPolicy: OldPolicy): NewPolicy | null => {
       {
         criterion: Criterion.CHECK_RESOURCE_INTEGRITY,
         args: null
-      },
-      {
-        criterion: Criterion.CHECK_NONCE_EXISTS,
-        args: null
       }
     ],
     then: ['approve', 'confirm'].includes(result) ? Then.PERMIT : Then.FORBID
   }
 
-  const chainId = chain_id !== '*' && chain_id !== null ? chain_id : '137'
+  const chainId = chain_id && chain_id !== '*' ? chain_id : '137'
 
   const currency = usd_amount ? FiatCurrency.USD : '*'
 
@@ -102,13 +100,6 @@ const translateLegacyPolicy = (oldPolicy: OldPolicy): NewPolicy | null => {
       })
     }
 
-    if (key === 'destination_address') {
-      res.when.push({
-        criterion: Criterion.CHECK_DESTINATION_ADDRESS,
-        args: [value]
-      })
-    }
-
     if (key === 'contract_hex_signature') {
       res.when.push({
         criterion: Criterion.CHECK_INTENT_HEX_SIGNATURE,
@@ -118,7 +109,7 @@ const translateLegacyPolicy = (oldPolicy: OldPolicy): NewPolicy | null => {
 
     if (key === 'activity_type') {
       let action = ''
-      let intent = ''
+      let intent: string[] = []
       let token = ''
       let contract = ''
       let spender = ''
@@ -128,12 +119,15 @@ const translateLegacyPolicy = (oldPolicy: OldPolicy): NewPolicy | null => {
 
       if (value === 'fungibleAssetTransfer') {
         action = 'signTransaction'
+        if (assetType === '*') {
+          intent = ['transferErc20', 'transferNative']
+        }
         if (assetType === 'erc20') {
-          intent = 'transferErc20'
+          intent = ['transferErc20']
           token = `eip155:${chainId}/${assetType}:${assetAddress}`
         }
         if (assetType === 'native') {
-          intent = 'transferNative'
+          intent = ['transferNative']
           if (chainId === '1') {
             token = `eip155:${chainId}/slip44:60`
           }
@@ -141,44 +135,59 @@ const translateLegacyPolicy = (oldPolicy: OldPolicy): NewPolicy | null => {
             token = `eip155:${chainId}/slip44:966`
           }
         }
+        if (destination_address && destination_address !== '*') {
+          res.when.push({
+            criterion: Criterion.CHECK_DESTINATION_ADDRESS,
+            args: [value]
+          })
+        }
       }
       if (value === 'nftAssetTransfer') {
         action = 'signTransaction'
+        if (assetType === '*') {
+          intent = ['transferErc721', 'transferErc1155']
+        }
         if (assetType === 'erc721') {
-          intent = 'transferErc721'
+          intent = ['transferErc721']
         }
         if (assetType === 'erc1155') {
-          intent = 'transferERC1155'
+          intent = ['transferErc1155']
         }
-        if (assetAddress !== null && assetAddress !== '*') {
-          contract = `eip155:${chainId}/${assetAddress}`
-          if (assetType !== null && assetType !== '*' && assetTokenId !== null && assetTokenId !== '*') {
+        if (assetAddress && assetAddress !== '*') {
+          contract = `eip155:${chainId}:${assetAddress}`
+          if (assetType && assetType !== '*' && assetTokenId && assetTokenId !== '*') {
             tokenId = `eip155:${chainId}/${assetType}:${assetAddress}/${assetTokenId}`
           }
+        }
+        if (destination_address && destination_address !== '*') {
+          res.when.push({
+            criterion: Criterion.CHECK_DESTINATION_ADDRESS,
+            args: [value]
+          })
         }
       }
       if (value === 'contractCall') {
         action = 'signTransaction'
-        intent = 'callContract'
+        intent = ['callContract']
         if (destination_account_type === 'contract' && destination_address !== '*') {
-          contract = `eip155:${chainId}/${destination_address}`
+          contract = `eip155:${chainId}:${destination_address}`
         }
       }
       if (value === 'tokenApproval') {
         action = 'signTransaction'
-        intent = 'approveTokenAllowance'
+        intent = ['approveTokenAllowance']
         token = `eip155:${chainId}/${assetType || 'erc20'}:${assetAddress}`
         if (destination_account_type === 'contract' && destination_address !== '*') {
-          spender = `eip155:${chainId}/${destination_address}`
+          spender = `eip155:${chainId}:${destination_address}`
         }
       }
       if (value === 'signMessage') {
         action = 'signMessage'
         if (signing_type === 'personalSign') {
-          intent = 'signMessage'
+          intent = ['signMessage']
         }
         if (signing_type === 'signTypedData') {
-          intent = 'signTypedData'
+          intent = ['signTypedData']
         }
       }
 
@@ -188,10 +197,10 @@ const translateLegacyPolicy = (oldPolicy: OldPolicy): NewPolicy | null => {
           args: [action]
         } as PolicyCriterion)
       }
-      if (intent) {
+      if (intent.length > 0) {
         when.push({
           criterion: Criterion.CHECK_INTENT_TYPE,
-          args: [intent]
+          args: intent
         } as PolicyCriterion)
       }
       if (token) {
@@ -213,13 +222,13 @@ const translateLegacyPolicy = (oldPolicy: OldPolicy): NewPolicy | null => {
         } as PolicyCriterion)
       }
       if (tokenId) {
-        if (intent === 'transferErc721') {
+        if (intent.includes('transferErc721')) {
           when.push({
             criterion: Criterion.CHECK_ERC721_TOKEN_ID,
             args: [tokenId]
           } as PolicyCriterion)
         }
-        if (intent === 'transferErc1155') {
+        if (intent.includes('transferErc1155')) {
           when.push({
             criterion: Criterion.CHECK_ERC1155_TOKEN_ID,
             args: [tokenId]
@@ -247,18 +256,18 @@ const translateLegacyPolicy = (oldPolicy: OldPolicy): NewPolicy | null => {
       })
     }
 
-    if (['domain_version', 'domain_name', 'domain_verifying_contract'].includes(key)) {
+    if (key === 'domain_version') {
       const args: SignTypedDataDomainCondition = {}
 
-      if (domain_version !== null && domain_version !== '*') {
+      if (domain_version && domain_version !== '*') {
         args['version'] = [domain_version]
       }
 
-      if (domain_name !== null && domain_name !== '*') {
+      if (domain_name && domain_name !== '*') {
         args['name'] = [domain_name]
       }
 
-      if (domain_verifying_contract !== null && domain_verifying_contract !== '*') {
+      if (domain_verifying_contract && domain_verifying_contract !== '*') {
         args['verifyingContract'] = [domain_verifying_contract as Address]
       }
 
@@ -279,18 +288,18 @@ const translateLegacyPolicy = (oldPolicy: OldPolicy): NewPolicy | null => {
       entityIds: []
     }
 
-    if (approval_threshold !== null && approval_threshold !== '*') {
+    if (approval_threshold && approval_threshold !== '*') {
       approval.approvalCount = Number(approval_threshold)
     }
-    if (approval_user_id !== null && approval_user_id !== '*') {
+    if (approval_user_id && approval_user_id !== '*') {
       approval.approvalEntityType = EntityType.User
       approval.entityIds = [approval_user_id]
     }
-    if (approval_user_role !== null && approval_user_role !== '*') {
+    if (approval_user_role && approval_user_role !== '*') {
       approval.approvalEntityType = EntityType.UserRole
       approval.entityIds = [approval_user_role]
     }
-    if (approval_user_group !== null && approval_user_group !== '*') {
+    if (approval_user_group && approval_user_group !== '*') {
       approval.approvalEntityType = EntityType.UserGroup
       approval.entityIds = [approval_user_group]
     }
@@ -306,44 +315,40 @@ const translateLegacyPolicy = (oldPolicy: OldPolicy): NewPolicy | null => {
   return res
 }
 
-export const sendTranslatingRequest = async (policies: OldPolicy[]) => {
-  try {
-    console.log(`Number of policies to translate: ${policies.length}.`)
-    console.log('Translation in progress...')
-    await axios.post('http://localhost:3010/admin/policies', {
-      authentication: {
-        sig: '0x746ed2e4bf7311da76bc157c7fe8c0520b6e4c27ab96abf5a8d16fecbaac98b669418b2db9da8e6d3cbd4e1eaff1a9d9e765f0470e9b86c6694145778a8d46f81c',
+export const run = () => {
+  const policies = data.policies.map((policy) => {
+    const copy = omit(policy, ['guild_id', 'sequence', 'version', 'amount']) as OldPolicy
+    copy.amount = policy.amount ? `${policy.amount}` : null
+    return copy
+  })
+  const requestData = policies.map(translateLegacyPolicy).filter(Boolean) as NewPolicy[]
+  console.log(`number of policies to translate: ${requestData.length}.`)
+  return axios.post('http://localhost:3010/admin/policies', {
+    authentication: {
+      sig: '0x746ed2e4bf7311da76bc157c7fe8c0520b6e4c27ab96abf5a8d16fecbaac98b669418b2db9da8e6d3cbd4e1eaff1a9d9e765f0470e9b86c6694145778a8d46f81c',
+      alg: 'ES256K',
+      pubKey: '0xd75D626a116D4a1959fE3bB938B2e7c116A05890'
+    },
+    approvals: [
+      {
+        sig: '0xe86dffd265b7a76a9de0ee9078137271cbe32bb2bb8ee28a2935cc37f023193a51cd608701b9c40fc42be69eeb45c0bb375b5898828f1af4bf12e37ff1fe697f1c',
         alg: 'ES256K',
-        pubKey: '0xd75D626a116D4a1959fE3bB938B2e7c116A05890'
+        pubKey: '0x501D5c2Ce1EF208aadf9131a98BAa593258CfA06'
       },
-      approvals: [
-        {
-          sig: '0xe86dffd265b7a76a9de0ee9078137271cbe32bb2bb8ee28a2935cc37f023193a51cd608701b9c40fc42be69eeb45c0bb375b5898828f1af4bf12e37ff1fe697f1c',
-          alg: 'ES256K',
-          pubKey: '0x501D5c2Ce1EF208aadf9131a98BAa593258CfA06'
-        },
-        {
-          sig: '0xaffbddca4f16079f86a56d58f9ebb151c353e73c11a09791eb97f01ea0046c545ea0bd765ab1dc844ee0369f9123476b6f84b00b42b7ac1a16676b9a11e1a4031c',
-          alg: 'ES256K',
-          pubKey: '0xab88c8785D0C00082dE75D801Fcb1d5066a6311e'
-        }
-      ],
-      request: {
-        action: 'setPolicyRules',
-        nonce: 'random-nonce-111',
-        data: policies.map(translateLegacyPolicy)
+      {
+        sig: '0xaffbddca4f16079f86a56d58f9ebb151c353e73c11a09791eb97f01ea0046c545ea0bd765ab1dc844ee0369f9123476b6f84b00b42b7ac1a16676b9a11e1a4031c',
+        alg: 'ES256K',
+        pubKey: '0xab88c8785D0C00082dE75D801Fcb1d5066a6311e'
       }
-    })
-    console.log('Translation completed!')
-  } catch (err) {
-    console.error(err.response.data)
-  }
+    ],
+    request: {
+      action: 'setPolicyRules',
+      nonce: 'random-nonce-111',
+      data: requestData
+    }
+  })
 }
 
-// sendTranslatingRequest(
-//   data.policies.map((policy) => {
-//     const copy: OldPolicy = omit(policy, ['guild_id', 'sequence', 'version', 'amount'])
-//     copy.amount = policy.amount !== null ? `${policy.amount}` : null
-//     return copy
-//   })
-// )
+run()
+  .then(() => console.log('done'))
+  .catch((error) => console.log('error', error))
