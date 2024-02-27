3356b3188877a51a94ce51134ae2e4c6ff029262
William Calderipe <wcalderipe@gmail.com>
2024-02-27 17:37:13 +0100

    Delete store module

--- apps/armory/src/store/store.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common'
-import { EntityStoreModule } from './entity/entity-store.module'
-
-@Module({
-  imports: [EntityStoreModule]
-})
-export class StoreModule {}
