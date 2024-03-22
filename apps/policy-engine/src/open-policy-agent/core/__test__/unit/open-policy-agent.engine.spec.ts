86e1643367ac4e8e386463427d22c82496e7a67f
Pierre Troger <pierre@narval.xyz>
2024-03-22 12:35:28 -0400

    reverting unecessary changes to test file

--- apps/policy-engine/src/open-policy-agent/core/__test__/unit/open-policy-agent.engine.spec.ts
+++ apps/policy-engine/src/open-policy-agent/core/__test__/unit/open-policy-agent.engine.spec.ts
@@ -149,17 +149,12 @@ describe('OpenPolicyAgentEngine', () => {
         resourceId: FIXTURE.WALLET.Engineering.id
       }
 
-      const authentication = await signJwt(
-        {
-          requestHash: hash(request),
-          sub: FIXTURE.USER.Alice.id
-        },
-        secp256k1PrivateKeyToJwk(FIXTURE.UNSAFE_PRIVATE_KEY.Alice),
-        { alg: SigningAlg.EIP191 },
-        buildSignerEip191(FIXTURE.UNSAFE_PRIVATE_KEY.Alice)
-      )
       const evaluation: EvaluationRequest = {
-        authentication,
+        authentication: await getJwt({
+          privateKey: FIXTURE.UNSAFE_PRIVATE_KEY.Alice,
+          sub: FIXTURE.USER.Alice.id,
+          request
+        }),
         request
       }
 
