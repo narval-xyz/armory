252372f89e0331fa24db58b74282eb9f6133dce0
Ptroger <44851272+Ptroger@users.noreply.github.com>
2024-03-25 11:02:43 -0400

    Jwk Generation
    * add jwk generation, rename generic generation function to alg specific name
    
    * add validation for public and private key
    
    * adding test coverage on key generation
    
    * wip fixing CI: signature invalid on fixture keys
    
    * fixed fixture data
    
    * reverting unecessary changes to test file
    
    * fixed rebase errors, added a 'use' field to the opts of key generation
    
    * removed unecessary console.log

--- packages/signature/src/lib/utils.ts
+++ packages/signature/src/lib/utils.ts
@@ -1,8 +1,26 @@
+import { p256 } from '@noble/curves/p256'
 import { secp256k1 } from '@noble/curves/secp256k1'
 import { sha256 as sha256Hash } from '@noble/hashes/sha256'
+import { exportJWK, generateKeyPair } from 'jose'
 import { toHex } from 'viem'
 import { publicKeyToAddress } from 'viem/utils'
-import { Alg, Curves, Hex, KeyTypes, Secp256k1KeySchema, Secp256k1PrivateKey, Secp256k1PublicKey } from './types'
+import { JwtError } from './error'
+import { rsaPrivateKeySchema } from './schemas'
+import {
+  Alg,
+  Curves,
+  Hex,
+  Jwk,
+  KeyTypes,
+  P256PrivateKey,
+  P256PublicKey,
+  RsaPrivateKey,
+  Secp256k1KeySchema,
+  Secp256k1PrivateKey,
+  Secp256k1PublicKey,
+  Use
+} from './types'
+import { validate } from './validate'
 
 export const algToJwk = (
   alg: Alg
@@ -55,6 +73,20 @@ export const secp256k1PublicKeyToJwk = (publicKey: Hex, keyId?: string): Secp256
   }
 }
 
+export const p256PublicKeyToJwk = (publicKey: Hex, keyId?: string): P256PublicKey => {
+  const hexPubKey = publicKey.slice(4)
+  const x = hexPubKey.slice(0, 64)
+  const y = hexPubKey.slice(64)
+  return {
+    kty: KeyTypes.EC,
+    crv: Curves.P256,
+    alg: Alg.ES256,
+    kid: keyId || publicKeyToAddress(publicKey),
+    x: hexToBase64Url(`0x${x}`),
+    y: hexToBase64Url(`0x${y}`)
+  }
+}
+
 // ES256k
 export const secp256k1PrivateKeyToJwk = (privateKey: Hex, keyId?: string): Secp256k1PrivateKey => {
   const publicKey = toHex(secp256k1.getPublicKey(privateKey.slice(2), false))
@@ -63,6 +95,19 @@ export const secp256k1PrivateKeyToJwk = (privateKey: Hex, keyId?: string): Secp2
     ...publicJwk,
     d: hexToBase64Url(privateKey)
   }
+}
+
+export const p256PrivateKeyToJwk = (privateKey: Hex, keyId?: string): P256PrivateKey => {
+  const publicKey = toHex(p256.getPublicKey(privateKey.slice(2), false))
+  const publicJwk = p256PublicKeyToJwk(publicKey, keyId)
+  return {
+    ...publicJwk,
+    d: hexToBase64Url(privateKey)
+  }
+}
+
+export const p256PrivateKeyToHex = (jwk: P256PrivateKey): Hex => {
+  return base64UrlToHex(jwk.d)
 }
 
 export const secp256k1PublicKeyToHex = (jwk: Secp256k1KeySchema): Hex => {
@@ -96,4 +141,70 @@ export const base64UrlToHex = (base64Url: string): Hex => {
 
 export const base64UrlToBytes = (base64Url: string): Buffer => {
   return Buffer.from(base64UrlToBase64(base64Url), 'base64')
+}
+
+const rsaKeyToKid = (jwk: Jwk) => {
+  // Concatenate the 'n' and 'e' values, splitted by ':'
+  const dataToHash = `${jwk.n}:${jwk.e}`
+
+  const binaryData = base64UrlToBytes(dataToHash)
+  const hash = sha256Hash(binaryData)
+  return toHex(hash)
+}
+
+const generateRsaKeyPair = async (
+  opts: {
+    keyId?: string
+    modulusLength?: number,
+    use?: Use,
+  } = {
+    modulusLength: 2048
+  }
+): Promise<RsaPrivateKey> => {
+  const { privateKey } = await generateKeyPair(Alg.RS256, {
+    modulusLength: opts.modulusLength,
+    extractable: true
+  })
+
+  const partialJwk = await exportJWK(privateKey)
+  if (!partialJwk.n) {
+    throw new JwtError({ message: 'Invalid JWK; missing n', context: { partialJwk } })
+  }
+  const jwk: Jwk = {
+    ...partialJwk,
+    alg: Alg.RS256,
+    kty: KeyTypes.RSA,
+    crv: undefined,
+    use: opts.use || undefined,
+  }
+  jwk.kid = opts.keyId || rsaKeyToKid(jwk);
+
+  const pk = validate<RsaPrivateKey>(rsaPrivateKeySchema, jwk, 'Invalid RSA Private Key JWK')
+  return pk
+}
+
+export const generateJwk = async (
+  alg: Alg,
+  opts?: {
+    keyId?: string
+    modulusLength?: number,
+    use?: Use,
+  }
+): Promise<Jwk> => {
+  switch (alg) {
+    case Alg.ES256K: {
+      const privateKeyK1 = toHex(secp256k1.utils.randomPrivateKey())
+      return secp256k1PrivateKeyToJwk(privateKeyK1, opts?.keyId)
+    }
+    case Alg.ES256: {
+      const privateKeyP256 = toHex(p256.utils.randomPrivateKey())
+      return p256PrivateKeyToJwk(privateKeyP256, opts?.keyId)
+    }
+    case Alg.RS256: {
+      const jwk = await generateRsaKeyPair(opts)
+      return jwk
+    }
+    default:
+      throw new Error(`Unsupported algorithm: ${alg}`)
+  }
 }
