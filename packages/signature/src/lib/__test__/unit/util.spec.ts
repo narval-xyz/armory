14f79dbc380a5152514355a4693e20ecbb8a77fd
Ptroger <44851272+Ptroger@users.noreply.github.com>
2024-03-25 11:02:43 -0400

    Jwk Generation
    * add jwk generation, rename generic generation function to alg specific name
    
    * add validation for public and private key
    
    * adding test coverage on key generation
    
    * wip fixing CI: signature invalid on fixture keys
    
    * fixed fixture data
    
    * reverting unecessary changes to test file
    
    * fixed rebase errors, added a 'use' field to the opts of key generation
    
    * removed unecessary console.log

--- packages/signature/src/lib/__test__/unit/util.spec.ts
+++ packages/signature/src/lib/__test__/unit/util.spec.ts
@@ -1,4 +1,10 @@
+import { p256PrivateKeySchema, rsaPrivateKeySchema, secp256k1PrivateKeySchema } from '../../schemas'
+import { buildSignerEip191, signJwt } from '../../sign'
 import { isHeader, isPayload } from '../../typeguards'
+import { Alg, Secp256k1PrivateKey, SigningAlg } from '../../types'
+import { generateJwk, secp256k1PrivateKeyToHex } from '../../utils'
+import { validate } from '../../validate'
+import { verifyJwt } from '../../verify'
 
 describe('isHeader', () => {
   it('returns true for a valid header object', () => {
@@ -57,5 +63,40 @@ describe('isPayload', () => {
 
   it('returns false for a non-object', () => {
     expect(isPayload('string')).toBe(false)
+  })
+})
+
+describe('generateKeys', () => {
+  it('generate a valid RSA key pair and return it as a JWK', async () => {
+    const key = await generateJwk(Alg.RS256)
+    expect(rsaPrivateKeySchema.safeParse(key).success).toBe(true)
+  })
+
+  it('generates a valid P-256 key pair and return it as a JWK', async () => {
+    const key = await generateJwk(Alg.ES256)
+    expect(p256PrivateKeySchema.safeParse(key).success).toBe(true)
+  })
+
+  it('generates a valid secp256k1 key pair and return it as a JWK', async () => {
+    const key = await generateJwk(Alg.ES256K)
+    expect(secp256k1PrivateKeySchema.safeParse(key).success).toBe(true)
+  })
+
+  it('can sign and verify with a generated secp256k1 key pair', async () => {
+    const key = await generateJwk(Alg.ES256K)
+    const message = 'test message'
+    const payload = {
+      requestHash: message
+    }
+    const validatedKey = validate<Secp256k1PrivateKey>(
+      secp256k1PrivateKeySchema,
+      key,
+      'Invalid secp256k1 Private Key JWK'
+    )
+
+    const signer = buildSignerEip191(secp256k1PrivateKeyToHex(validatedKey))
+    const signature = await signJwt(payload, key, { alg: SigningAlg.EIP191 }, signer)
+    const isValid = await verifyJwt(signature, key)
+    expect(isValid).not.toEqual(false)
   })
 })
