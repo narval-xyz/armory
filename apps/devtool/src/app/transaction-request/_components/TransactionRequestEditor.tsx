22b21bf63321574f1056baa1fbc90de815fcbe7b
samuel <samuel@narval.xyz>
2024-03-28 15:26:32 +0100

    fix

--- apps/devtool/src/app/transaction-request/_components/TransactionRequestEditor.tsx
+++ apps/devtool/src/app/transaction-request/_components/TransactionRequestEditor.tsx
@@ -2,6 +2,7 @@
 
 import { Editor } from '@monaco-editor/react'
 import { useRef, useState } from 'react'
+import NarButton from '../../_design-system/NarButton'
 
 const TransactionRequestEditor = () => {
   const [data, setData] = useState<string>()
@@ -10,17 +11,20 @@ const TransactionRequestEditor = () => {
   const monacoRef = useRef<any>(null)
 
   return (
-    <div className="border-2 border-white rounded-xl p-4 w-2/3">
-      <Editor
-        height="70vh"
-        language="json"
-        value={data}
-        onChange={(value) => setData(value)}
-        onMount={(editor, monaco) => {
-          editorRef.current = editor
-          monacoRef.current = monaco
-        }}
-      />
+    <div className="flex gap-12">
+      <div className="border-2 border-white rounded-xl p-4 w-2/3">
+        <Editor
+          height="70vh"
+          language="json"
+          value={data}
+          onChange={(value) => setData(value)}
+          onMount={(editor, monaco) => {
+            editorRef.current = editor
+            monacoRef.current = monaco
+          }}
+        />
+      </div>
+      <NarButton label="Send" />
     </div>
   )
 }
