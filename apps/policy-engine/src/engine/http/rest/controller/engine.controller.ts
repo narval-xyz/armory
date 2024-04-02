9fc8324402bea9cd2d78f80fd030e23b58a3ac37
samuel <samuel@narval.xyz>
2024-04-02 15:39:37 +0200

    fix

--- apps/policy-engine/src/engine/http/rest/controller/engine.controller.ts
+++ apps/policy-engine/src/engine/http/rest/controller/engine.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, HttpCode, HttpStatus, UseGuards } from '@nestjs/common'
-import { ClientSecretGuard } from 'apps/policy-engine/src/shared/guard/client-secret.guard'
+import { ClientSecretGuard } from '../../../../shared/guard/client-secret.guard'
 import { EngineSignerConfigService } from '../../../core/service/engine-signer-config.service'
 
 @Controller('/engine')
