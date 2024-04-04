1c09f6febfc615eb7e6d4c00ef610322fb4d1f17
William Calderipe <wcalderipe@gmail.com>
2024-04-04 10:18:04 -0300

    Support sign message

--- apps/policy-engine/src/shared/testing/evaluation.testing.ts
+++ apps/policy-engine/src/shared/testing/evaluation.testing.ts
@@ -1,11 +1,25 @@
 import { Action, EvaluationRequest, FIXTURE, Request, TransactionRequest } from '@narval/policy-engine-shared'
 import { Alg, Payload, hash, privateKeyToJwk, signJwt } from '@narval/signature'
 import { UNSAFE_PRIVATE_KEY } from 'packages/policy-engine-shared/src/lib/dev.fixture'
+import { v4 as uuid } from 'uuid'
 import { toHex } from 'viem'
 
 export const ONE_ETH = BigInt('1000000000000000000')
 
-export const generateInboundEvaluationRequest = async (): Promise<EvaluationRequest> => {
+const sign = async (request: Request) => {
+  const message = hash(request)
+  const payload: Payload = {
+    requestHash: message
+  }
+
+  const aliceSignature = await signJwt(payload, privateKeyToJwk(UNSAFE_PRIVATE_KEY.Alice, Alg.ES256K))
+  const bobSignature = await signJwt(payload, privateKeyToJwk(UNSAFE_PRIVATE_KEY.Bob, Alg.ES256K))
+  const carolSignature = await signJwt(payload, privateKeyToJwk(UNSAFE_PRIVATE_KEY.Carol, Alg.ES256K))
+
+  return { aliceSignature, bobSignature, carolSignature }
+}
+
+export const generateSignTransactionRequest = async (): Promise<EvaluationRequest> => {
   const txRequest: TransactionRequest = {
     from: FIXTURE.WALLET.Engineering.address,
     to: FIXTURE.WALLET.Treasury.address,
@@ -23,14 +37,24 @@ export const generateInboundEvaluationRequest = async (): Promise<EvaluationRequ
     resourceId: FIXTURE.WALLET.Engineering.id
   }
 
-  const message = hash(request)
-  const payload: Payload = {
-    requestHash: message
+  const { aliceSignature, bobSignature, carolSignature } = await sign(request)
+
+  return {
+    authentication: aliceSignature,
+    request,
+    approvals: [bobSignature, carolSignature]
+  }
+}
+
+export const generateSignMessageRequest = async (): Promise<EvaluationRequest> => {
+  const request: Request = {
+    action: Action.SIGN_MESSAGE,
+    nonce: uuid(),
+    resourceId: FIXTURE.WALLET.Engineering.id,
+    message: 'generated sign message request'
   }
 
-  const aliceSignature = await signJwt(payload, privateKeyToJwk(UNSAFE_PRIVATE_KEY.Alice, Alg.ES256K))
-  const bobSignature = await signJwt(payload, privateKeyToJwk(UNSAFE_PRIVATE_KEY.Bob, Alg.ES256K))
-  const carolSignature = await signJwt(payload, privateKeyToJwk(UNSAFE_PRIVATE_KEY.Carol, Alg.ES256K))
+  const { aliceSignature, bobSignature, carolSignature } = await sign(request)
 
   return {
     authentication: aliceSignature,
