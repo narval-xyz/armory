4a1d03e966c3c4f7118a833b4718154796c6f1c4
William Calderipe <wcalderipe@gmail.com>
2024-04-17 14:07:59 -0300

    Add users EOA JWK credentials in the fixtures (#232)
    * Add user EOA JWK credentials fixtures
    
    Refactor the fixtures policies.
    
    Refactor the devtool to use the fixtures.
    
    Add examples in the devtool.
    
    Fix the wallet repository to write and read lower case ids.
    
    * Fix tests

--- apps/devtool/src/app/request-playground/_components/PlaygroundEditor.tsx
+++ apps/devtool/src/app/request-playground/_components/PlaygroundEditor.tsx
@@ -12,18 +12,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Editor } from '@monaco-editor/react'
 import { Decision, EvaluationRequest, EvaluationResponse } from '@narval/policy-engine-shared'
 import { hash } from '@narval/signature'
-import { useMemo, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import NarButton from '../../_design-system/NarButton'
 import useAccountSignature from '../../_hooks/useAccountSignature'
 import useEngineApi from '../../_hooks/useEngineApi'
 import useVaultApi from '../../_hooks/useVaultApi'
-import example from './example.json'
+import { erc20, spendingLimits } from './request'
 
 const PlaygroundEditor = () => {
   const { signAccountJwt } = useAccountSignature()
   const { errors: evaluationErrors, evaluateRequest } = useEngineApi()
   const { errors: signatureErrors, signTransaction } = useVaultApi()
-  const [codeEditor, setCodeEditor] = useState<string | undefined>(JSON.stringify(example, null, 2))
+  const [codeEditor, setCodeEditor] = useState<string | undefined>()
   const [isProcessing, setIsProcessing] = useState<boolean>(false)
   const [evaluationResponse, setEvaluationResponse] = useState<EvaluationResponse>()
   const [signature, setSignature] = useState<string>()
@@ -102,19 +102,49 @@ const PlaygroundEditor = () => {
     setIsProcessing(false)
   }
 
+  const updateExample = async () => {
+    setCodeEditor(JSON.stringify(await erc20(), null, 2))
+  }
+
+  useEffect(() => {
+    if (codeEditor) return
+
+    updateExample()
+  }, [codeEditor])
+
   return (
     <div className="flex items-start gap-12">
-      <div className="border-2 border-white rounded-xl p-4 w-2/3">
-        <Editor
-          height="70vh"
-          language="json"
-          value={codeEditor}
-          onChange={(value) => setCodeEditor(value)}
-          onMount={(editor, monaco) => {
-            editorRef.current = editor
-            monacoRef.current = monaco
-          }}
-        />
+      <div className="w-2/3">
+        <div className="border-2 border-white rounded-xl p-4">
+          <Editor
+            height="70vh"
+            language="json"
+            value={codeEditor}
+            options={{
+              minimap: {
+                enabled: false
+              }
+            }}
+            onChange={(value) => setCodeEditor(value)}
+            onMount={(editor, monaco) => {
+              editorRef.current = editor
+              monacoRef.current = monaco
+            }}
+          />
+        </div>
+        <div className="pt-4">
+          <h3 className="pb-4">Examples</h3>
+          <div className="flex gap-5 ">
+            <NarButton
+              label="ERC-20"
+              onClick={async () => setCodeEditor(JSON.stringify(await erc20(), null, 2))}
+            />
+            <NarButton
+              label="Spending limits"
+              onClick={async () => setCodeEditor(JSON.stringify(await spendingLimits(), null, 2))}
+            />
+          </div>
+        </div>
       </div>
       <div className="flex flex-col gap-5 w-1/3">
         <div className="flex items-center gap-4">
@@ -154,7 +184,11 @@ const PlaygroundEditor = () => {
             <pre>{JSON.stringify(evaluationResponse, null, 3)}</pre>
           </div>
         )}
-        {!isProcessing && !error && signature && <div className="text-nv-white truncate">{signature}</div>}
+        {!isProcessing && !error && signature && (
+          <div className="border-2 border-white rounded-t-xl p-4 overflow-clip">
+            <pre>{signature}</pre>
+          </div>
+        )}
       </div>
     </div>
   )
---
+++ apps/vault/src/vault/persistence/repository/__test__/unit/wallet.repository.spec.ts
@@ -0,0 +1,89 @@
+import { EncryptionModule, EncryptionService } from '@narval/encryption-module'
+import { Test } from '@nestjs/testing'
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts'
+import { KeyValueRepository } from '../../../../../shared/module/key-value/core/repository/key-value.repository'
+import { EncryptKeyValueService } from '../../../../../shared/module/key-value/core/service/encrypt-key-value.service'
+import { InMemoryKeyValueRepository } from '../../../../../shared/module/key-value/persistence/repository/in-memory-key-value.repository'
+import { getTestRawAesKeyring } from '../../../../../shared/testing/encryption.testing'
+import { Wallet } from '../../../../../shared/type/domain.type'
+import { WalletRepository } from '../../wallet.repository'
+
+describe(WalletRepository.name, () => {
+  let repository: WalletRepository
+  let inMemoryKeyValueRepository: InMemoryKeyValueRepository
+  let encryptionService: EncryptionService
+
+  beforeEach(async () => {
+    inMemoryKeyValueRepository = new InMemoryKeyValueRepository()
+
+    const module = await Test.createTestingModule({
+      imports: [
+        EncryptionModule.register({
+          keyring: getTestRawAesKeyring()
+        })
+      ],
+      providers: [
+        EncryptKeyValueService,
+        WalletRepository,
+        {
+          provide: KeyValueRepository,
+          useValue: inMemoryKeyValueRepository
+        }
+      ]
+    }).compile()
+
+    repository = module.get<WalletRepository>(WalletRepository)
+    encryptionService = module.get<EncryptionService>(EncryptionService)
+  })
+
+  describe('save', () => {
+    const clientId = 'test-client-id'
+    const privateKey = generatePrivateKey()
+    const account = privateKeyToAccount(privateKey)
+    const wallet: Wallet = {
+      id: 'test-WALLET-ID',
+      privateKey,
+      address: account.address
+    }
+
+    it('uses lower case id in the key', async () => {
+      jest.spyOn(inMemoryKeyValueRepository, 'set')
+
+      await repository.save(clientId, wallet)
+
+      expect(inMemoryKeyValueRepository.set).toHaveBeenCalledWith(
+        `wallet:${clientId}:${wallet.id.toLowerCase()}`,
+        expect.any(String)
+      )
+    })
+
+    it('encrypts wallet data', async () => {
+      await repository.save(clientId, wallet)
+
+      const actualEncryptedWallet = await inMemoryKeyValueRepository.get(
+        `wallet:${clientId}:${wallet.id.toLowerCase()}`
+      )
+
+      const actualWallet = await encryptionService
+        .decrypt(Buffer.from(actualEncryptedWallet as string, 'hex'))
+        .then((v) => JSON.parse(v.toString()))
+
+      expect(actualWallet).toEqual(wallet)
+    })
+  })
+
+  describe('findById', () => {
+    it('uses lower case id', async () => {
+      jest.spyOn(inMemoryKeyValueRepository, 'get')
+
+      const clientId = 'test-client-id'
+      const caseSensitiveWalletId = 'test-WALLET-ID'
+
+      await repository.findById(clientId, caseSensitiveWalletId)
+
+      expect(inMemoryKeyValueRepository.get).toHaveBeenCalledWith(
+        `wallet:${clientId}:${caseSensitiveWalletId.toLowerCase()}`
+      )
+    })
+  })
+})
