a474848bcab8b0847a225ee68a24faf336ef686e
William Calderipe <wcalderipe@gmail.com>
2024-05-02 12:12:46 +0200

    Add nonce guard in the sign controller
    Save used nonces in the sign service

--- apps/vault/src/vault/__test__/e2e/sign.spec.ts
+++ apps/vault/src/vault/__test__/e2e/sign.spec.ts
@@ -14,30 +14,33 @@ import {
   type Payload
 } from '@narval/signature'
 import { HttpStatus, INestApplication } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
 import { ACCOUNT, UNSAFE_PRIVATE_KEY } from 'packages/policy-engine-shared/src/lib/dev.fixture'
 import request from 'supertest'
 import { v4 as uuid } from 'uuid'
 import { verifyMessage } from 'viem'
-import { ClientModule } from '../../../client/client.module'
 import { ClientService } from '../../../client/core/service/client.service'
-import { load } from '../../../main.config'
 import { REQUEST_HEADER_CLIENT_ID } from '../../../main.constant'
 import { KeyValueRepository } from '../../../shared/module/key-value/core/repository/key-value.repository'
 import { InMemoryKeyValueRepository } from '../../../shared/module/key-value/persistence/repository/in-memory-key-value.repository'
+import { TestPrismaService } from '../../../shared/module/persistence/service/test-prisma.service'
 import { getTestRawAesKeyring } from '../../../shared/testing/encryption.testing'
 import { Client, Wallet } from '../../../shared/type/domain.type'
+import { AppService } from '../../core/service/app.service'
 import { WalletRepository } from '../../persistence/repository/wallet.repository'
+import { VaultModule } from '../../vault.module'
+
+const PRIVATE_KEY = '0x7cfef3303797cbc7515d9ce22ffe849c701b0f2812f999b0847229c47951fca5'
 
 describe('Sign', () => {
   let app: INestApplication
   let module: TestingModule
+  let testPrismaService: TestPrismaService
 
   const adminApiKey = 'test-admin-api-key'
+
   const clientId = uuid()
 
-  const PRIVATE_KEY = '0x7cfef3303797cbc7515d9ce22ffe849c701b0f2812f999b0847229c47951fca5'
   // Engine key used to sign the approval request
   const enginePrivateJwk = secp256k1PrivateKeyToJwk(PRIVATE_KEY)
   const clientPublicJWK = secp256k1PrivateKeyToPublicJwk(PRIVATE_KEY)
@@ -56,9 +59,9 @@ describe('Sign', () => {
     privateKey: '0x7cfef3303797cbc7515d9ce22ffe849c701b0f2812f999b0847229c47951fca5'
   }
 
-  const defaultRequest = {
-    action: 'signTransaction',
-    nonce: 'random-nonce-111',
+  const getSignTransactionRequest = (nonce?: string) => ({
+    action: Action.SIGN_TRANSACTION,
+    nonce: nonce || uuid(),
     transactionRequest: {
       from: '0x2c4895215973CbBd778C32c456C074b99daF8Bf1',
       to: '0x04B12F0863b83c7162429f0Ebb0DfdA20E1aA97B',
@@ -72,32 +75,24 @@ describe('Sign', () => {
       maxPriorityFeePerGas: '81000000000'
     },
     resourceId: 'eip155:eoa:0x2c4895215973CbBd778C32c456C074b99daF8Bf1'
-  }
+  })
 
-  const getAccessToken = async (request?: unknown, opts: object = {}) => {
+  const getAccessToken = async (request: unknown, opts: object = {}) => {
     const payload: Payload = {
-      requestHash: hash(request || defaultRequest),
+      requestHash: hash(request),
       sub: 'test-root-user-uid',
       iss: 'https://armory.narval.xyz',
       iat: Math.floor(Date.now() / 1000),
       ...opts
     }
-
     const signer = buildSignerEip191(PRIVATE_KEY)
-    const jwt = await signJwt(payload, enginePrivateJwk, { alg: SigningAlg.EIP191 }, signer)
 
-    return jwt
+    return signJwt(payload, enginePrivateJwk, { alg: SigningAlg.EIP191 }, signer)
   }
 
   beforeAll(async () => {
     module = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({
-          load: [load],
-          isGlobal: true
-        }),
-        ClientModule
-      ]
+      imports: [VaultModule]
     })
       .overrideProvider(KeyValueRepository)
       .useValue(new InMemoryKeyValueRepository())
@@ -105,44 +100,48 @@ describe('Sign', () => {
       .useValue({
         keyring: getTestRawAesKeyring()
       })
-      .overrideProvider(ClientService)
-      .useValue({
-        findAll: jest.fn().mockResolvedValue([client]),
-        findByClientId: jest.fn().mockResolvedValue(client)
-      })
-      .overrideProvider(WalletRepository)
-      .useValue({
-        findById: jest.fn().mockResolvedValue(wallet)
-      })
       .compile()
 
     app = module.createNestApplication({ logger: false })
+
+    testPrismaService = module.get<TestPrismaService>(TestPrismaService)
+
+    const appService = module.get<AppService>(AppService)
+    const clientService = module.get<ClientService>(ClientService)
+    const walletRepository = module.get<WalletRepository>(WalletRepository)
+
+    await appService.save({
+      id: 'test-app',
+      masterKey: 'unsafe-test-master-key',
+      adminApiKey
+    })
+
+    await clientService.onboard(client)
+
+    await walletRepository.save(clientId, wallet)
+
+    await testPrismaService.truncateAll()
 
     await app.init()
   })
 
   afterAll(async () => {
-    // await testPrismaService.truncateAll()
+    await testPrismaService.truncateAll()
     await module.close()
     await app.close()
   })
 
   describe('POST /sign', () => {
     it('has client secret guard', async () => {
-      const { status } = await request(app.getHttpServer())
-        .post('/sign')
-        // .set(REQUEST_HEADER_CLIENT_ID, clientId)  NO CLIENT SECRET
-        .send({})
+      const { status } = await request(app.getHttpServer()).post('/sign').send({})
 
       expect(status).toEqual(HttpStatus.UNPROCESSABLE_ENTITY)
     })
 
-    it('validates nested txn data', async () => {
-      // ValidationPipe & Transforms can easily be implemented incorrectly, so make sure this is running.
-
+    it('validates nested transaction data', async () => {
       const payload = {
         request: {
-          action: 'signTransaction',
+          action: Action.SIGN_TRANSACTION,
           nonce: 'random-nonce-111',
           transactionRequest: {
             from: '0x2c4895215973CbBd778C32c456C074b99daF8Bf1',
@@ -179,28 +178,27 @@ describe('Sign', () => {
     })
 
     it('signs', async () => {
-      const bodyPayload = { request: defaultRequest }
+      const payload = { request: getSignTransactionRequest() }
 
-      const accessToken = await getAccessToken()
+      const accessToken = await getAccessToken(payload.request)
 
       const { status, body } = await request(app.getHttpServer())
         .post('/sign')
         .set(REQUEST_HEADER_CLIENT_ID, clientId)
         .set('authorization', `GNAP ${accessToken}`)
-        .send(bodyPayload)
+        .send(payload)
 
       expect(status).toEqual(HttpStatus.CREATED)
-
       expect(body).toEqual({
         signature:
           '0x02f875818982013d8512dbf9ea008543cb655fef82520c9404b12f0863b83c7162429f0ebb0dfda20e1aa97b865af3107a400080c080a00de78cbb96f83ef1b8d6be4d55b4046b2706c7d63ce0a815bae2b1ea4f891e6ba06f7648a9c9710b171d55e056c4abca268857f607a8a4a257d945fc44ace9f076'
       })
     })
 
-    it('signs Message', async () => {
+    it('signs message', async () => {
       const messageRequest = {
         action: Action.SIGN_MESSAGE,
-        nonce: 'random-nonce-111',
+        nonce: uuid(),
         message: 'My ASCII message',
         resourceId: 'eip155:eoa:0x2c4895215973CbBd778C32c456C074b99daF8Bf1'
       }
@@ -222,29 +220,62 @@ describe('Sign', () => {
         signature:
           '0x65071b7126abd24fe6b8fa396529e21d22448d23ff1a6c5a0e043a4f641cd11b2a21958127d1b91db4d991f8b33ad6b201637799a95eadbe3a7cf5cee26bd9521b'
       })
-
       expect(status).toEqual(HttpStatus.CREATED)
-
       expect(isVerified).toEqual(true)
     })
-  })
 
-  describe('AuthorizationGuard', () => {
-    it('returns error when request does not match authorized request', async () => {
-      const bodyPayload = {
-        request: {
-          ...defaultRequest,
-          nonce: defaultRequest.nonce + 'x' // CHANGE THE NONCE SO IT DOES NOT MATCH ACCESS TOKEN
-        }
+    it('returns error when nonce is used twice', async () => {
+      const messageRequest = {
+        action: Action.SIGN_MESSAGE,
+        nonce: 'random-nonce-111',
+        message: 'My ASCII message',
+        resourceId: 'eip155:eoa:0x2c4895215973CbBd778C32c456C074b99daF8Bf1'
       }
 
-      const accessToken = await getAccessToken(defaultRequest)
+      const accessToken = await getAccessToken(messageRequest)
+
+      await request(app.getHttpServer())
+        .post('/sign')
+        .set(REQUEST_HEADER_CLIENT_ID, clientId)
+        .set('authorization', `GNAP ${accessToken}`)
+        .send({ request: messageRequest })
 
       const { status, body } = await request(app.getHttpServer())
         .post('/sign')
         .set(REQUEST_HEADER_CLIENT_ID, clientId)
         .set('authorization', `GNAP ${accessToken}`)
-        .send(bodyPayload)
+        .send({ request: messageRequest })
+
+      expect(body).toMatchObject({
+        statusCode: HttpStatus.FORBIDDEN,
+        message: 'Nonce already used',
+        context: {
+          clientId,
+          nonce: messageRequest.nonce
+        },
+        stack: expect.any(String)
+      })
+      expect(status).toEqual(HttpStatus.FORBIDDEN)
+    })
+  })
+
+  describe('authorization', () => {
+    it('returns error when request does not match authorized request', async () => {
+      const payload = getSignTransactionRequest()
+      const tamperedPayload = {
+        request: {
+          ...payload,
+          nonce: payload.nonce + 'x'
+        }
+      }
+
+      const accessToken = await getAccessToken(payload)
+
+      const { status, body } = await request(app.getHttpServer())
+        .post('/sign')
+        .set(REQUEST_HEADER_CLIENT_ID, clientId)
+        .set('authorization', `GNAP ${accessToken}`)
+        .send(tamperedPayload)
 
       expect(body).toEqual({
         message: 'Invalid request hash',
@@ -257,16 +288,16 @@ describe('Sign', () => {
 
     describe('jwsd', () => {
       it('returns error when auth is client-bound but no jwsd header', async () => {
-        const bodyPayload = { request: defaultRequest }
+        const payload = { request: getSignTransactionRequest() }
 
         const clientJwk = secp256k1PublicKeyToJwk(ACCOUNT.Alice.publicKey)
-        const accessToken = await getAccessToken(defaultRequest, { cnf: clientJwk })
+        const accessToken = await getAccessToken(payload.request, { cnf: clientJwk })
 
         const { status, body } = await request(app.getHttpServer())
           .post('/sign')
           .set(REQUEST_HEADER_CLIENT_ID, clientId)
           .set('authorization', `GNAP ${accessToken}`)
-          .send(bodyPayload)
+          .send(payload)
 
         expect(status).toEqual(HttpStatus.FORBIDDEN)
         expect(body.statusCode).toEqual(HttpStatus.FORBIDDEN)
@@ -275,10 +306,10 @@ describe('Sign', () => {
 
       it('verifies jwsd header in a client-bound request', async () => {
         const now = Math.floor(Date.now() / 1000)
-        const bodyPayload = { request: defaultRequest }
+        const payload = { request: getSignTransactionRequest() }
 
         const clientJwk = secp256k1PublicKeyToJwk(ACCOUNT.Alice.publicKey)
-        const accessToken = await getAccessToken(defaultRequest, { cnf: clientJwk })
+        const accessToken = await getAccessToken(payload.request, { cnf: clientJwk })
 
         const jwsdSigner = buildSignerEip191(UNSAFE_PRIVATE_KEY.Alice)
         const jwsdHeader: JwsdHeader = {
@@ -290,7 +321,7 @@ describe('Sign', () => {
           created: now,
           ath: hexToBase64Url(hash(accessToken))
         }
-        const jwsd = await signJwsd(bodyPayload, jwsdHeader, jwsdSigner).then((jws) => {
+        const jwsd = await signJwsd(payload, jwsdHeader, jwsdSigner).then((jws) => {
           // Strip out the middle part for size
           const parts = jws.split('.')
           parts[1] = ''
@@ -302,7 +333,7 @@ describe('Sign', () => {
           .set(REQUEST_HEADER_CLIENT_ID, clientId)
           .set('authorization', `GNAP ${accessToken}`)
           .set('detached-jws', jwsd)
-          .send(bodyPayload)
+          .send(payload)
 
         expect(body).toEqual({
           signature: expect.any(String)
@@ -312,13 +343,13 @@ describe('Sign', () => {
 
       it('returns error when auth is client-bound to a different key', async () => {
         const now = Math.floor(Date.now() / 1000)
-        const bodyPayload = { request: defaultRequest }
+        const payload = { request: getSignTransactionRequest() }
 
         const clientJwk = secp256k1PublicKeyToJwk(ACCOUNT.Alice.publicKey)
         const boundClientJwk = secp256k1PublicKeyToJwk(ACCOUNT.Bob.publicKey)
         // We bind BOB to the access token, but ALICe is the one signing the request, so she has
         // a valid access token but it's not bound to her.
-        const accessToken = await getAccessToken(defaultRequest, { cnf: boundClientJwk })
+        const accessToken = await getAccessToken(payload.request, { cnf: boundClientJwk })
 
         const jwsdSigner = buildSignerEip191(UNSAFE_PRIVATE_KEY.Alice)
         const jwsdHeader: JwsdHeader = {
@@ -331,7 +362,7 @@ describe('Sign', () => {
           ath: ''
         }
 
-        const jwsd = await signJwsd(bodyPayload, jwsdHeader, jwsdSigner).then((jws) => {
+        const jwsd = await signJwsd(payload, jwsdHeader, jwsdSigner).then((jws) => {
           // Strip out the middle part for size
           const parts = jws.split('.')
           parts[1] = ''
@@ -343,7 +374,7 @@ describe('Sign', () => {
           .set(REQUEST_HEADER_CLIENT_ID, clientId)
           .set('authorization', `GNAP ${accessToken}`)
           .set('detached-jws', jwsd)
-          .send(bodyPayload)
+          .send(payload)
 
         expect(status).toEqual(HttpStatus.FORBIDDEN)
         expect(body.statusCode).toEqual(HttpStatus.FORBIDDEN)
