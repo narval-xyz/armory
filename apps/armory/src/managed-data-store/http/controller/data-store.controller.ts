9c91f9da277a03cc63fb96c959111198f03c28bb
Samuel <samuel@narval.xyz>
2024-05-07 16:53:15 +0200

    Pass the clientId through query params instead of headers (#247)

--- apps/armory/src/managed-data-store/http/controller/data-store.controller.ts
+++ apps/armory/src/managed-data-store/http/controller/data-store.controller.ts
@@ -1,11 +1,10 @@
 import { Entities, EntityStore, JwtString, Policy, PolicyStore } from '@narval/policy-engine-shared'
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post, Query } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
-import { OrgId } from '../../../shared/decorator/org-id.decorator'
 import { EntityDataStoreService } from '../../core/service/entity-data-store.service'
 import { PolicyDataStoreService } from '../../core/service/policy-data-store.service'
 
-@Controller('/managed-data-store')
+@Controller('/data')
 @ApiTags('Managed Data Store')
 export class DataStoreController {
   constructor(
@@ -14,24 +13,24 @@ export class DataStoreController {
   ) {}
 
   @Get('/entities')
-  async getEntities(@OrgId() orgId: string): Promise<{ entity: EntityStore } | null> {
-    const entity = await this.entityDataStoreService.getEntities(orgId)
+  async getEntities(@Query('clientId') clientId: string): Promise<{ entity: EntityStore } | null> {
+    const entity = await this.entityDataStoreService.getEntities(clientId)
     return entity ? { entity } : null
   }
 
   @Get('/policies')
-  async getPolicies(@OrgId() orgId: string): Promise<{ policy: PolicyStore } | null> {
-    const policy = await this.policyDataStoreService.getPolicies(orgId)
+  async getPolicies(@Query('clientId') clientId: string): Promise<{ policy: PolicyStore } | null> {
+    const policy = await this.policyDataStoreService.getPolicies(clientId)
     return policy ? { policy } : null
   }
 
   @Post('/entities')
-  setEntities(@OrgId() orgId: string, @Body() payload: { signature: JwtString; data: Entities }) {
-    return this.entityDataStoreService.setEntities(orgId, payload)
+  setEntities(@Query('clientId') clientId: string, @Body() payload: { signature: JwtString; data: Entities }) {
+    return this.entityDataStoreService.setEntities(clientId, payload)
   }
 
   @Post('/policies')
-  setPolicies(@OrgId() orgId: string, @Body() payload: { signature: JwtString; data: Policy[] }) {
-    return this.policyDataStoreService.setPolicies(orgId, payload)
+  setPolicies(@Query('clientId') clientId: string, @Body() payload: { signature: JwtString; data: Policy[] }) {
+    return this.policyDataStoreService.setPolicies(clientId, payload)
   }
 }
