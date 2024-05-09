3846b694a607cdcf8531a32d958fc7eb5fcd485b
Matt Schoch <matt@narval.xyz>
2024-05-09 14:16:39 -0400

    missed test

--- apps/policy-engine/src/shared/module/persistence/seed.ts
+++ apps/policy-engine/src/shared/module/persistence/seed.ts
@@ -7,8 +7,7 @@ const prisma = new PrismaClient()
 const engine: Engine = {
   id: '7d704a62-d15e-4382-a826-1eb41563043b',
   adminApiKey: 'admin-api-key-xxx',
-  masterKey: 'master-key-xxx',
-  activated: true
+  masterKey: 'master-key-xxx'
 }
 
 async function main() {
