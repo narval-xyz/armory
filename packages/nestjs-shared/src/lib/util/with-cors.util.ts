ae9c33734b5fa5ba60cbe8bc4584d8a9df32087d
Matt Schoch <matt@narval.xyz>
2024-05-09 14:48:03 -0400

    Opening up CORS; do not restrict public interfaces

--- packages/nestjs-shared/src/lib/util/with-cors.util.ts
+++ packages/nestjs-shared/src/lib/util/with-cors.util.ts
@@ -3,9 +3,14 @@ import { INestApplication } from '@nestjs/common'
 export const withCors =
   (origin: string[] | undefined) =>
   (app: INestApplication): INestApplication => {
-    if (origin && origin.length > 0) {
-      app.enableCors({ origin })
-    }
+    app.enableCors({
+      origin: origin && !!origin.length ? origin : '*', // Default to open, but allow closing it up if desired.
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: false,
+      allowedHeaders: '*'
+    })
 
     return app
   }
