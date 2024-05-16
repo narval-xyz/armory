a4ca8eec325b20213bd1af48b0e8d0f121d78d34
William Calderipe <wcalderipe@gmail.com>
2024-05-16 17:44:42 +0200

    Use sign transaction request to test bigint coercion

--- apps/armory/src/policy-engine/core/service/__test__/integration/cluster.service.spec.ts
+++ apps/armory/src/policy-engine/core/service/__test__/integration/cluster.service.spec.ts
@@ -1,14 +1,12 @@
 import { ConfigModule } from '@narval/config-module'
 import { secret } from '@narval/nestjs-shared'
 import {
-  Action,
   DataStoreConfiguration,
   Decision,
   EvaluationRequest,
   EvaluationResponse,
   PublicClient,
   Request,
-  SignMessageAction,
   Source,
   SourceType
 } from '@narval/policy-engine-shared'
@@ -19,6 +17,7 @@ import { Test } from '@nestjs/testing'
 import nock from 'nock'
 import { v4 as uuid } from 'uuid'
 import { generatePrivateKey } from 'viem/accounts'
+import { generateSignTransactionRequest } from '../../../../../__test__/fixture/authorization-request.fixture'
 import { load } from '../../../../../armory.config'
 import { PersistenceModule } from '../../../../../shared/module/persistence/persistence.module'
 import { TestPrismaService } from '../../../../../shared/module/persistence/service/test-prisma.service'
@@ -132,12 +131,7 @@ describe(ClusterService.name, () => {
       attestationPrivateKey: privateKeyToJwk(generatePrivateKey())
     }
 
-    const signTransaction: SignMessageAction = {
-      action: Action.SIGN_MESSAGE,
-      nonce: uuid(),
-      resourceId: 'test-resource-id',
-      message: 'sign me'
-    }
+    const signTransaction = generateSignTransactionRequest()
 
     const clientId = 'test-client-id'
 
