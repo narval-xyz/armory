1e7e9a6623b9acdd0abd89e4de92befb1f65bd68
Matt Schoch <matt@narval.xyz>
2024-05-21 09:40:34 -0400

    Removing EngineSignerConfig; Signer is client-specific not global for the engine (#262)

--- apps/policy-engine/src/engine/http/rest/controller/engine.controller.ts
+++ apps/policy-engine/src/engine/http/rest/controller/engine.controller.ts
@@ -1,15 +1,13 @@
 import { Controller, Get, HttpCode, HttpStatus, UseGuards } from '@nestjs/common'
 import { ClientSecretGuard } from '../../../../shared/guard/client-secret.guard'
-import { EngineSignerConfigService } from '../../../core/service/engine-signer-config.service'
 
 @Controller('/engine')
 export class EngineController {
-  constructor(private engineSignerConfigService: EngineSignerConfigService) {}
-
   @Get()
   @HttpCode(HttpStatus.OK)
   @UseGuards(ClientSecretGuard)
-  async getPublicJwk() {
-    return this.engineSignerConfigService.getPublicJwkOrThrow()
+  async getApp() {
+    // TODO: this endpoint should return basic Engine configuration; nothing client-specific, but the server itself.
+    return 'Engine is running'
   }
 }
