d599e7b5a3fec2c8f75b1ce59abd8568b681924c
William Calderipe <wcalderipe@gmail.com>
2024-05-22 17:50:29 +0200

    Disable ESLint error in the config module

--- packages/config-module/src/lib/config.service.ts
+++ packages/config-module/src/lib/config.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common'
+// eslint-disable-next-line no-restricted-imports
 import { ConfigService as NestConfigService, Path, PathValue } from '@nestjs/config'
 
 /**
