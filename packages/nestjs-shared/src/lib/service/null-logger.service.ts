6c04eb6551efbd7eda7dfdb1232625fc1c7b60bc
William Calderipe <wcalderipe@gmail.com>
2024-05-22 17:56:07 +0200

    Disable lint rules in the null logger

--- packages/nestjs-shared/src/lib/service/null-logger.service.ts
+++ packages/nestjs-shared/src/lib/service/null-logger.service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { LoggerService } from '@nestjs/common'
 
 export class NullLoggerService implements LoggerService {
