a3aada041b5ef9b3396f4daa2fbc4f2d3e403e3c
Matt Schoch <matt@narval.xyz>
2024-05-23 19:18:48 -0400

    Hacking in Vault Sign on Authorization page

--- apps/devtool/src/app/auth-server/_components/AuthServerPlayground.tsx
+++ apps/devtool/src/app/auth-server/_components/AuthServerPlayground.tsx
@@ -1,10 +1,23 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import Playground from '../../_components/Playground'
 import useAuthServerApi from '../../_hooks/useAuthServerApi'
+import useVaultApi from '../../_hooks/useVaultApi'
 
 const AuthServerPlayground = () => {
-  const { errors, authorizationResponse, authorize } = useAuthServerApi()
+  const { errors: evaluationErrors, authorizationResponse, authorize } = useAuthServerApi()
+  const { errors: signatureErrors, sign, importPk } = useVaultApi()
+
+  const [errors, setErrors] = useState<string>()
+
+  useEffect(() => {
+    if (evaluationErrors) {
+      setErrors(evaluationErrors)
+    } else if (signatureErrors) {
+      setErrors(signatureErrors)
+    }
+  }, [evaluationErrors, signatureErrors])
 
   return (
     <Playground
@@ -12,6 +25,8 @@ const AuthServerPlayground = () => {
       errors={errors}
       response={authorizationResponse ? JSON.stringify(authorizationResponse, null, 2) : undefined}
       authorize={authorize}
+      sign={sign}
+      importPk={importPk}
     />
   )
 }
