c76cad140402cb7e48ac517c83a1c5eebf2320b2
William Calderipe <wcalderipe@gmail.com>
2024-05-24 09:36:42 +0200

    Refactor authorization request types to use Zod

--- apps/armory/src/orchestration/core/type/domain.type.ts
+++ apps/armory/src/orchestration/core/type/domain.type.ts
@@ -1,59 +1,80 @@
 import { Action, JwtString, TransactionRequest } from '@narval/policy-engine-shared'
 import { OverrideProperties, SetOptional } from 'type-fest'
+import { z } from 'zod'
 
-export enum AuthorizationRequestStatus {
-  CREATED = 'CREATED',
-  CANCELED = 'CANCELED',
-  FAILED = 'FAILED',
-  PROCESSING = 'PROCESSING',
-  APPROVING = 'APPROVING',
-  PERMITTED = 'PERMITTED',
-  FORBIDDEN = 'FORBIDDEN'
-}
+export const AuthorizationRequestStatus = {
+  CREATED: 'CREATED',
+  CANCELED: 'CANCELED',
+  FAILED: 'FAILED',
+  PROCESSING: 'PROCESSING',
+  APPROVING: 'APPROVING',
+  PERMITTED: 'PERMITTED',
+  FORBIDDEN: 'FORBIDDEN'
+} as const
+export type AuthorizationRequestStatus = (typeof AuthorizationRequestStatus)[keyof typeof AuthorizationRequestStatus]
 
-export type Evaluation = {
-  id: string
-  decision: string
-  signature: string | null
-  createdAt: Date
-}
+export const Evaluation = z.object({
+  id: z.string(),
+  decision: z.string(),
+  signature: z.string().nullable(),
+  createdAt: z.date()
+})
+export type Evaluation = z.infer<typeof Evaluation>
 
 /**
  * AuthZ actions currently supported by the Armory.
  */
-export type SupportedAction = typeof Action.SIGN_TRANSACTION | typeof Action.SIGN_MESSAGE
+export const SupportedAction = {
+  SIGN_TRANSACTION: Action.SIGN_TRANSACTION,
+  SIGN_MESSAGE: Action.SIGN_MESSAGE
+} as const
+export type SupportedAction = (typeof SupportedAction)[keyof typeof SupportedAction]
 
-export type SharedAuthorizationPayload = {
-  action: SupportedAction
-  nonce: string
-}
+const SharedAuthorizationPayload = z.object({
+  action: z.nativeEnum(SupportedAction),
+  nonce: z.string()
+})
+type SharedAuthorizationPayload = z.infer<typeof SharedAuthorizationPayload>
 
-export type SignTransaction = SharedAuthorizationPayload & {
-  action: typeof Action.SIGN_TRANSACTION
-  resourceId: string
+export const SignTransaction = SharedAuthorizationPayload.extend({
+  action: z.literal(Action.SIGN_TRANSACTION),
+  resourceId: z.string(),
   transactionRequest: TransactionRequest
-}
+})
+export type SignTransaction = z.infer<typeof SignTransaction>
 
-export type SignMessage = SharedAuthorizationPayload & {
-  action: typeof Action.SIGN_MESSAGE
-  resourceId: string
-  message: string
-}
+export const SignMessage = SharedAuthorizationPayload.extend({
+  action: z.literal(Action.SIGN_MESSAGE),
+  resourceId: z.string(),
+  message: z.string()
+})
+export type SignMessage = z.infer<typeof SignMessage>
 
-export type Request = SignTransaction | SignMessage
+export const Request = z.discriminatedUnion('action', [SignTransaction, SignMessage])
+export type Request = z.infer<typeof Request>
 
-export type AuthorizationRequest = {
-  id: string
-  clientId: string
-  status: `${AuthorizationRequestStatus}`
-  authentication: JwtString
-  request: Request
-  approvals: JwtString[]
-  evaluations: Evaluation[]
-  idempotencyKey?: string | null
-  createdAt: Date
-  updatedAt: Date
-}
+export const AuthorizationRequestError = z.object({
+  context: z.record(z.string()).optional(),
+  id: z.string(),
+  message: z.string(),
+  name: z.string()
+})
+export type AuthorizationRequestError = z.infer<typeof AuthorizationRequestError>
+
+export const AuthorizationRequest = z.object({
+  approvals: z.array(JwtString),
+  authentication: JwtString,
+  clientId: z.string(),
+  createdAt: z.date(),
+  errors: z.array(AuthorizationRequestError).optional(),
+  evaluations: z.array(Evaluation),
+  id: z.string(),
+  idempotencyKey: z.string().nullish(),
+  request: Request,
+  status: z.nativeEnum(AuthorizationRequestStatus),
+  updatedAt: z.date()
+})
+export type AuthorizationRequest = z.infer<typeof AuthorizationRequest>
 
 export type CreateAuthorizationRequest = OverrideProperties<
   SetOptional<AuthorizationRequest, 'id' | 'status' | 'createdAt' | 'updatedAt'>,
