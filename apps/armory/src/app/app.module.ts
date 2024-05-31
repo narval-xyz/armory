c50fdccf7ef3b02e036a175ff7122eba3e0bad78
William Calderipe <wcalderipe@gmail.com>
2024-05-31 11:06:16 +0200

    Add admin API key in the Armory (#285)

---
+++ apps/armory/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { PersistenceModule } from '../shared/module/persistence/persistence.module'
+import { AppService } from './core/service/app.service'
+import { AppController } from './http/rest/controller/app.controller'
+import { AppRepository } from './persistence/repository/app.repository'
+
+@Module({
+  imports: [PersistenceModule],
+  providers: [AppService, AppRepository],
+  controllers: [AppController]
+})
+export class AppModule {}
---
+++ apps/armory/src/app/http/rest/controller/app.controller.ts
@@ -0,0 +1,49 @@
+import { secret } from '@narval/nestjs-shared'
+import { Controller, HttpStatus, Post } from '@nestjs/common'
+import { ApiExcludeController } from '@nestjs/swagger'
+import { ApplicationException } from '../../../../shared/exception/application.exception'
+import { AlreadyActivatedException } from '../../../core/exception/app-already-activated.exception'
+import { AppService } from '../../../core/service/app.service'
+
+type ActivateResponse =
+  | { state: 'ACTIVATED' }
+  | {
+      state: 'READY'
+      app: {
+        appId: string
+        adminApiKey?: string
+      }
+    }
+
+@Controller('/apps')
+@ApiExcludeController()
+export class AppController {
+  constructor(private appService: AppService) {}
+
+  @Post('/activate')
+  async activate(): Promise<ActivateResponse> {
+    const adminApiKey = secret.generate()
+
+    try {
+      const app = await this.appService.activate(adminApiKey)
+
+      return {
+        state: 'READY',
+        app: {
+          appId: app.id,
+          adminApiKey
+        }
+      }
+    } catch (error) {
+      if (error instanceof AlreadyActivatedException) {
+        return { state: 'ACTIVATED' }
+      }
+
+      throw new ApplicationException({
+        message: 'Something went wrong during the app activation',
+        suggestedHttpStatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        origin: error
+      })
+    }
+  }
+}
