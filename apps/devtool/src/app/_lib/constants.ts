3dedda9656d1f6539f1ad50f0d18dc990049f865
samuel <samuel@narval.xyz>
2024-06-05 11:12:48 +0200

    fix bug

--- apps/devtool/src/app/_lib/constants.ts
+++ apps/devtool/src/app/_lib/constants.ts
@@ -1,9 +1,10 @@
+export const LOCAL_DATA_STORE_URL = 'http://localhost:4200/api/data-store'
 export const AUTH_SERVER_URL = 'http://localhost:3005'
 export const ENGINE_URL = 'http://localhost:3010'
 export const VAULT_URL = 'http://localhost:3011'
-export const MANAGED_ENTITY_DATA_STORE_URL = `${AUTH_SERVER_URL}/data/entities?clientId=`
-export const MANAGED_POLICY_DATA_STORE_URL = `${AUTH_SERVER_URL}/data/policies?clientId=`
-export const LOCAL_DATA_STORE_URL = 'http://localhost:4200/api/data-store'
+
+export const MANAGED_ENTITY_DATA_STORE_PATH = 'data/entities?clientId='
+export const MANAGED_POLICY_DATA_STORE_PATH = 'data/policies?clientId='
 
 export const LOCAL_STORAGE_KEYS = {
   authServerUrl: 'narvalAuthServerUrl',
