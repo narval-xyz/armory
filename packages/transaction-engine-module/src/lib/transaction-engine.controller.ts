bad5874c487d1a1b743fe332831f91e4c87733a0
William Calderipe <wcalderipe@gmail.com>
2024-06-07 17:36:36 +0200

    Delete transaction engine module

--- packages/transaction-engine-module/src/lib/transaction-engine.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common'
-
-@Controller('transaction-engine')
-export class TransactionEngineController {
-  @Get()
-  hello() {
-    return { message: 'Hello Transaction Engine Module' }
-  }
-}
