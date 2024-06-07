8bb896709013c6ff6eacb11cbb5ea0635cb83bc2
William Calderipe <wcalderipe@gmail.com>
2024-06-07 18:23:37 +0200

    Delete transaction engine template module (#316)
    * Delete transaction engine module
    
    * Remove references from package-lock.json

--- packages/transaction-engine-module/src/lib/transaction-engine.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { TransactionEngineController } from './transaction-engine.controller'
-
-@Module({
-  controllers: [TransactionEngineController],
-  providers: [],
-  exports: []
-})
-export class TransactionEngineModule {}
