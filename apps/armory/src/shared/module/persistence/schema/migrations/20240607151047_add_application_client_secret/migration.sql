1e883916f23b24bd2e1683c9cf88fef83f341b8a
samuel <samuel@narval.xyz>
2024-06-07 19:43:12 +0200

    fix

--- apps/armory/src/shared/module/persistence/schema/migrations/20240607151047_add_application_client_secret/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "application" ADD COLUMN     "client_secret" TEXT;
-UPDATE "application" SET client_secret = '******';
-ALTER TABLE "application" SET COLUMN     "client_secret" TEXT NOT NULL;
