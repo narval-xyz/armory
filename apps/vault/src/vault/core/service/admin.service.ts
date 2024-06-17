199b1068bb7317ebe082c46bf97f43ffaf982e94
Ptroger <44851272+Ptroger@users.noreply.github.com>
2024-06-17 15:41:44 +0200

    new endpoint to list wallets (#342)
    * new endpoint to list wallets
    
    * revised naming for Wallet and added guard on endpoint

--- apps/vault/src/vault/core/service/admin.service.ts
+++ apps/vault/src/vault/core/service/admin.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@nestjs/common'
 import { AccountRepository } from '../../persistence/repository/account.repository'
+import { RootKeyRepository } from '../../persistence/repository/root-key.repository'
 
 @Injectable()
 export class AdminService {
-  constructor(private accountRepository: AccountRepository) {}
+  constructor(
+    private accountRepository: AccountRepository,
+    private rootKeyRepository: RootKeyRepository
+  ) {}
 
   async getAccounts(clientId: string) {
     const accounts = await this.accountRepository.findByClientId(clientId)
     return accounts
+  }
+
+  async getWallets(clientId: string) {
+    const seeds = await this.rootKeyRepository.findByClientId(clientId)
+    return seeds
   }
 }
