f31f9ad235d6207cb214bcdf8d87b1536c35bad2
samuel <samuel@narval.xyz>
2024-06-20 15:13:58 +0200

    fix

--- apps/policy-engine/src/engine/persistence/repository/http-data-store.repository.ts
+++ apps/policy-engine/src/engine/persistence/repository/http-data-store.repository.ts
@@ -5,12 +5,14 @@ import axiosRetry from 'axios-retry'
 import { DataStoreException } from '../../core/exception/data-store.exception'
 import { DataStoreRepository } from '../../core/repository/data-store.repository'
 
+const MAX_RETRIES = 3
+
 @Injectable()
 export class HttpDataStoreRepository implements DataStoreRepository {
   async fetch<Data>(source: HttpSource): Promise<Data> {
     try {
       const client = axios.create()
-      axiosRetry(client, { retries: 3, retryDelay: axiosRetry.exponentialDelay })
+      axiosRetry(client, { retries: MAX_RETRIES, retryDelay: axiosRetry.exponentialDelay })
       const { data } = await client.get<Data>(source.url, { headers: source.headers })
 
       return data
