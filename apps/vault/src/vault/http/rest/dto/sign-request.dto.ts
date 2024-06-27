d2d32b28cfb5098e7df15a56b4f068c253514682
samuel <samuel@narval.xyz>
2024-06-27 07:55:14 +0200

    fix

--- apps/vault/src/vault/http/rest/dto/sign-request.dto.ts
+++ apps/vault/src/vault/http/rest/dto/sign-request.dto.ts
@@ -1,5 +1,5 @@
-import { createZodDto } from 'nestjs-zod'
 import { SerializedSignableRequest } from '@narval/policy-engine-shared'
+import { createZodDto } from 'nestjs-zod'
 import { z } from 'zod'
 
 export class SignRequestDto extends createZodDto(
