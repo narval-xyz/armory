f92a3fc352aafaec08aecc346cb51cc14c532a92
samuel <samuel@narval.xyz>
2024-06-28 16:15:08 +0200

    fix

--- packages/nestjs-shared/src/lib/middleware/http-logger-middleware.ts
+++ packages/nestjs-shared/src/lib/middleware/http-logger-middleware.ts
@@ -1,4 +1,5 @@
-import { Injectable, LoggerService, NestMiddleware } from '@nestjs/common'
+import { LoggerService } from '@narval/nestjs-shared'
+import { Injectable, NestMiddleware } from '@nestjs/common'
 import { NextFunction, Request, Response } from 'express'
 
 @Injectable()
