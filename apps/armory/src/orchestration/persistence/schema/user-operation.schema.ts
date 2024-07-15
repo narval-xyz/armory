20f7a3a816c7d08ff3f9c48521c5898004a8e102
Pierre Troger <pierre@narval.xyz>
2024-07-15 14:26:02 +0200

    read/create type are serialized unserialized for user operation

--- apps/armory/src/orchestration/persistence/schema/user-operation.schema.ts
+++ apps/armory/src/orchestration/persistence/schema/user-operation.schema.ts
@@ -1,21 +1,5 @@
-import { addressSchema, hexSchema } from '@narval/policy-engine-shared'
-import { z } from 'zod'
+import { SerializedUserOperationV6, UserOperationV6 } from '@narval/policy-engine-shared'
 
-export const readUserOperationSchema = z.object({
-  sender: addressSchema,
-  nonce: z.coerce.number(),
-  initCode: hexSchema,
-  callData: hexSchema,
-  callGasLimit: z.coerce.number(),
-  verificationGasLimit: z.coerce.number(),
-  preVerificationGas: z.coerce.number(),
-  maxFeePerGas: z.coerce.number(),
-  maxPriorityFeePerGas: z.coerce.number(),
-  paymasterAndData: hexSchema,
-  entryPoint: addressSchema,
-  signature: hexSchema,
-  factoryAddress: addressSchema,
-  chainId: z.coerce.number()
-})
+export const readUserOperationSchema = UserOperationV6
 
-export const createUserOperationSchema = readUserOperationSchema
+export const createUserOperationSchema = SerializedUserOperationV6
