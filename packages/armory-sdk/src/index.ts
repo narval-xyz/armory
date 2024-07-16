b3e8811773cd8567d04b80722db8e5e6f04d9100
William Calderipe <wcalderipe@gmail.com>
2024-07-16 14:51:15 +0200

    Remove lib directory from the SDK

--- packages/armory-sdk/src/index.ts
+++ packages/armory-sdk/src/index.ts
@@ -1,51 +1,7 @@
-export * from './lib/auth'
-export * from './lib/data-store'
-export * from './lib/domain'
-export * from './lib/http'
-export * from './lib/sdk'
-export * from './lib/shared/promise'
-export * from './lib/types'
-export * from './lib/vault'
+export * from './domain'
+export * from './http'
+export * from './sdk'
+export * from './shared/promise'
+export * from './types'
 
-export { resourceId } from './lib/utils'
-
-export {
-  Alg,
-  SigningAlg,
-  base64UrlToHex,
-  buildSignerEip191,
-  buildSignerForAlg,
-  eip191Hash,
-  getPublicKey,
-  hash,
-  hexToBase64Url,
-  jwkSchema,
-  privateKeyToJwk,
-  publicKeyToJwk,
-  signJwt
-} from '@narval/signature'
-
-export type { PrivateKey, PublicKey, RsaPublicKey } from '@narval/signature'
-
-export {
-  AccessToken,
-  AccountType,
-  Action,
-  CreateAuthorizationRequest,
-  Criterion,
-  Decision,
-  Eip712TypedData,
-  EntityType,
-  EntityUtil,
-  JwtString,
-  PolicyCriterion,
-  Request,
-  Then,
-  TransactionRequest,
-  UserRole,
-  ValueOperators,
-  getAddress,
-  toHex
-} from '@narval/policy-engine-shared'
-
-export type { AccountEntity, Address, Entities, Hex, Policy, UserEntity } from '@narval/policy-engine-shared'
+export { resourceId } from './utils'
