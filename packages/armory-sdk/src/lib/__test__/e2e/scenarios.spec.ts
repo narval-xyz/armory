bc8c8a9c3f6d6845c4ca84f4e3c385f57f0d7161
Pierre Troger <pierre@narval.xyz>
2024-08-26 13:40:39 +0200

    renamed test suites

--- packages/armory-sdk/src/lib/__test__/e2e/scenarios.spec.ts
+++ packages/armory-sdk/src/lib/__test__/e2e/scenarios.spec.ts
@@ -30,7 +30,7 @@ const alicePrivateKey = FIXTURE.UNSAFE_PRIVATE_KEY.Alice
 const carolPrivateKey = FIXTURE.UNSAFE_PRIVATE_KEY.Carol
 
 describe('End to end scenarios', () => {
-  describe('rate limiting', () => {
+  describe('rate limiting by principal', () => {
     const request: Request = {
       action: Action.SIGN_TRANSACTION,
       nonce: 'test-nonce',
@@ -219,224 +219,7 @@ describe('End to end scenarios', () => {
     })
   })
 
-  describe('spending limits', () => {
-    const request: Request = {
-      action: Action.SIGN_TRANSACTION,
-      nonce: 'test-nonce',
-      transactionRequest: {
-        from: '0x0301e2724a40E934Cce3345928b88956901aA127',
-        to: '0x76d1b7f9b3F69C435eeF76a98A415332084A856F',
-        value: '0x429D069189E0000', // 0.3 ETH
-        chainId: 1
-      },
-      resourceId: 'eip155:eoa:0x0301e2724a40e934cce3345928b88956901aa127'
-    }
-
-    // Generate a new client ID for each test run, otherwise historical data with persist between tests if using a long-lived db.
-    const clientId = v4()
-
-    beforeAll(async () => {
-      const entities: Entities = {
-        addressBook: [
-          {
-            id: 'eip155:1:0x9f38879167acCf7401351027EE3f9247A71cd0c5',
-            address: '0x9f38879167acCf7401351027EE3f9247A71cd0c5',
-            chainId: 1,
-            classification: 'internal'
-          },
-          {
-            id: 'eip155:1:0x0f610AC9F0091f8F573c33f15155afE8aD747495',
-            address: '0x0f610AC9F0091f8F573c33f15155afE8aD747495',
-            chainId: 1,
-            classification: 'counterparty'
-          }
-        ],
-        credentials: [
-          {
-            userId: 'test-alice-user-uid',
-            id: '0x4fca4ebdd44d54a470a273cb6c131303892cb754f0d374a860fab7936bb95d94',
-            key: {
-              kty: 'EC',
-              alg: 'ES256K',
-              kid: '0x4fca4ebdd44d54a470a273cb6c131303892cb754f0d374a860fab7936bb95d94',
-              crv: 'secp256k1',
-              x: 'zb-LwlHDtp5sV8E33k3H2TCm-LNTGIcFjODNWI4gHRY',
-              y: '6Pbt6dwxAeS7yHp7YV2GbXs_Px0tWrTfeTv9erjC7zs'
-            }
-          },
-          {
-            userId: 'test-bob-user-uid',
-            id: '0x7e431d5b570ba38e2e036387a596219ae9076e8a488a6149b491892b03582166',
-            key: {
-              kty: 'EC',
-              crv: 'secp256k1',
-              alg: 'ES256K',
-              kid: '0x7e431d5b570ba38e2e036387a596219ae9076e8a488a6149b491892b03582166',
-              x: 'm5zj9v8I_UvB-15y7t7RmQXmyNmPuvAQPDdU71LRkUA',
-              y: 'Az5R7PGJbmKdPpK2-jmUh7xyuaOZlCIFNU4I83xy5lU'
-            }
-          }
-        ],
-        tokens: [],
-        userGroupMembers: [],
-        userGroups: [],
-        userAccounts: [],
-        users: [
-          {
-            id: 'test-alice-user-uid',
-            role: 'admin'
-          },
-          {
-            id: 'test-bob-user-uid',
-            role: 'member'
-          }
-        ],
-        accountGroupMembers: [],
-        accountGroups: [],
-        accounts: [
-          {
-            id: 'eip155:eoa:0x0301e2724a40e934cce3345928b88956901aa127',
-            address: '0x0301e2724a40e934cce3345928b88956901aa127',
-            accountType: 'eoa'
-          },
-          {
-            id: 'eip155:eoa:0x76d1b7f9b3f69c435eef76a98a415332084a856f',
-            address: '0x76d1b7f9b3f69c435eef76a98a415332084a856f',
-            accountType: 'eoa'
-          }
-        ]
-      }
-      const policies: Policy[] = [
-        {
-          id: '1-admin-can-do-anything',
-          description: 'admin can do any action',
-          when: [
-            {
-              criterion: 'checkPrincipalRole',
-              args: ['admin']
-            }
-          ],
-          then: 'permit'
-        },
-        {
-          id: 'member-can-transfer-1-eth',
-          description: 'member can transfer 1 ETH',
-          when: [
-            {
-              criterion: 'checkAction',
-              args: ['signTransaction']
-            },
-            {
-              criterion: 'checkPrincipalRole',
-              args: ['member']
-            },
-            {
-              criterion: 'checkIntentType',
-              args: ['transferNative']
-            },
-            {
-              criterion: 'checkIntentToken',
-              args: ['eip155:1/slip44:60']
-            },
-            {
-              criterion: 'checkSpendingLimit',
-              args: {
-                limit: '1000000000000000000',
-                operator: 'lte' as ValueOperators,
-                timeWindow: {
-                  type: 'rolling',
-                  value: 86400
-                },
-                filters: {
-                  perPrincipal: true,
-                  tokens: ['eip155:1/slip44:60']
-                }
-              }
-            }
-          ],
-          then: 'permit'
-        }
-      ]
-
-      await createClient(systemManagerHexPk, {
-        clientId,
-        authHost: getAuthHost(),
-        authAdminApiKey: getAuthAdminApiKey()
-      })
-      await saveDataStore(systemManagerHexPk, {
-        clientId,
-        host: getAuthHost(),
-        entities,
-        policies
-      })
-    })
-
-    it('alice-admin does a transfer that is not counted against the rate limit', async () => {
-      const { authClient } = await buildAuthClient(alicePrivateKey, {
-        host: getAuthHost(),
-        clientId
-      })
-
-      const response = await authClient.requestAccessToken(request)
-      expect(response).toMatchObject({ value: expect.any(String) })
-    })
-
-    it('permits member bob to do a transfer', async () => {
-      const { authClient } = await buildAuthClient(bobPrivateKey, {
-        host: getAuthHost(),
-        clientId
-      })
-
-      const response = await authClient.requestAccessToken(request)
-      expect(response).toMatchObject({ value: expect.any(String) })
-    })
-
-    it('permits member bob to do a second transfer', async () => {
-      const { authClient } = await buildAuthClient(bobPrivateKey, {
-        host: getAuthHost(),
-        clientId
-      })
-
-      const response = await authClient.requestAccessToken(request)
-      expect(response).toMatchObject({ value: expect.any(String) })
-    })
-
-    it('permits member bob to do a third transfer', async () => {
-      const { authClient } = await buildAuthClient(bobPrivateKey, {
-        host: getAuthHost(),
-        clientId
-      })
-
-      const response = await authClient.requestAccessToken(request)
-      expect(response).toMatchObject({ value: expect.any(String) })
-    })
-
-    it('forbids member bob to exceed the limit', async () => {
-      expect.assertions(1)
-      const { authClient } = await buildAuthClient(bobPrivateKey, {
-        host: getAuthHost(),
-        clientId
-      })
-
-      try {
-        await authClient.requestAccessToken(request)
-      } catch (error: any) {
-        expect(error.message).toEqual('Unauthorized')
-      }
-    })
-
-    it('permits admin alice to do a transfer', async () => {
-      const { authClient } = await buildAuthClient(alicePrivateKey, {
-        host: getAuthHost(),
-        clientId
-      })
-
-      const response = await authClient.requestAccessToken(request)
-      expect(response).toMatchObject({ value: expect.any(String) })
-    })
-  })
-
-  describe('group rate limits', () => {
+  describe('rate limiting by groupId', () => {
     const request: Request = {
       action: Action.SIGN_TRANSACTION,
       nonce: 'test-nonce',
@@ -680,7 +463,224 @@ describe('End to end scenarios', () => {
     })
   })
 
-  describe('group spending limits requires an approval', () => {
+  describe('spending limits', () => {
+    const request: Request = {
+      action: Action.SIGN_TRANSACTION,
+      nonce: 'test-nonce',
+      transactionRequest: {
+        from: '0x0301e2724a40E934Cce3345928b88956901aA127',
+        to: '0x76d1b7f9b3F69C435eeF76a98A415332084A856F',
+        value: '0x429D069189E0000', // 0.3 ETH
+        chainId: 1
+      },
+      resourceId: 'eip155:eoa:0x0301e2724a40e934cce3345928b88956901aa127'
+    }
+
+    // Generate a new client ID for each test run, otherwise historical data with persist between tests if using a long-lived db.
+    const clientId = v4()
+
+    beforeAll(async () => {
+      const entities: Entities = {
+        addressBook: [
+          {
+            id: 'eip155:1:0x9f38879167acCf7401351027EE3f9247A71cd0c5',
+            address: '0x9f38879167acCf7401351027EE3f9247A71cd0c5',
+            chainId: 1,
+            classification: 'internal'
+          },
+          {
+            id: 'eip155:1:0x0f610AC9F0091f8F573c33f15155afE8aD747495',
+            address: '0x0f610AC9F0091f8F573c33f15155afE8aD747495',
+            chainId: 1,
+            classification: 'counterparty'
+          }
+        ],
+        credentials: [
+          {
+            userId: 'test-alice-user-uid',
+            id: '0x4fca4ebdd44d54a470a273cb6c131303892cb754f0d374a860fab7936bb95d94',
+            key: {
+              kty: 'EC',
+              alg: 'ES256K',
+              kid: '0x4fca4ebdd44d54a470a273cb6c131303892cb754f0d374a860fab7936bb95d94',
+              crv: 'secp256k1',
+              x: 'zb-LwlHDtp5sV8E33k3H2TCm-LNTGIcFjODNWI4gHRY',
+              y: '6Pbt6dwxAeS7yHp7YV2GbXs_Px0tWrTfeTv9erjC7zs'
+            }
+          },
+          {
+            userId: 'test-bob-user-uid',
+            id: '0x7e431d5b570ba38e2e036387a596219ae9076e8a488a6149b491892b03582166',
+            key: {
+              kty: 'EC',
+              crv: 'secp256k1',
+              alg: 'ES256K',
+              kid: '0x7e431d5b570ba38e2e036387a596219ae9076e8a488a6149b491892b03582166',
+              x: 'm5zj9v8I_UvB-15y7t7RmQXmyNmPuvAQPDdU71LRkUA',
+              y: 'Az5R7PGJbmKdPpK2-jmUh7xyuaOZlCIFNU4I83xy5lU'
+            }
+          }
+        ],
+        tokens: [],
+        userGroupMembers: [],
+        userGroups: [],
+        userAccounts: [],
+        users: [
+          {
+            id: 'test-alice-user-uid',
+            role: 'admin'
+          },
+          {
+            id: 'test-bob-user-uid',
+            role: 'member'
+          }
+        ],
+        accountGroupMembers: [],
+        accountGroups: [],
+        accounts: [
+          {
+            id: 'eip155:eoa:0x0301e2724a40e934cce3345928b88956901aa127',
+            address: '0x0301e2724a40e934cce3345928b88956901aa127',
+            accountType: 'eoa'
+          },
+          {
+            id: 'eip155:eoa:0x76d1b7f9b3f69c435eef76a98a415332084a856f',
+            address: '0x76d1b7f9b3f69c435eef76a98a415332084a856f',
+            accountType: 'eoa'
+          }
+        ]
+      }
+      const policies: Policy[] = [
+        {
+          id: '1-admin-can-do-anything',
+          description: 'admin can do any action',
+          when: [
+            {
+              criterion: 'checkPrincipalRole',
+              args: ['admin']
+            }
+          ],
+          then: 'permit'
+        },
+        {
+          id: 'member-can-transfer-1-eth',
+          description: 'member can transfer 1 ETH',
+          when: [
+            {
+              criterion: 'checkAction',
+              args: ['signTransaction']
+            },
+            {
+              criterion: 'checkPrincipalRole',
+              args: ['member']
+            },
+            {
+              criterion: 'checkIntentType',
+              args: ['transferNative']
+            },
+            {
+              criterion: 'checkIntentToken',
+              args: ['eip155:1/slip44:60']
+            },
+            {
+              criterion: 'checkSpendingLimit',
+              args: {
+                limit: '1000000000000000000',
+                operator: 'lte' as ValueOperators,
+                timeWindow: {
+                  type: 'rolling',
+                  value: 86400
+                },
+                filters: {
+                  perPrincipal: true,
+                  tokens: ['eip155:1/slip44:60']
+                }
+              }
+            }
+          ],
+          then: 'permit'
+        }
+      ]
+
+      await createClient(systemManagerHexPk, {
+        clientId,
+        authHost: getAuthHost(),
+        authAdminApiKey: getAuthAdminApiKey()
+      })
+      await saveDataStore(systemManagerHexPk, {
+        clientId,
+        host: getAuthHost(),
+        entities,
+        policies
+      })
+    })
+
+    it('alice-admin does a transfer that is not counted against the rate limit', async () => {
+      const { authClient } = await buildAuthClient(alicePrivateKey, {
+        host: getAuthHost(),
+        clientId
+      })
+
+      const response = await authClient.requestAccessToken(request)
+      expect(response).toMatchObject({ value: expect.any(String) })
+    })
+
+    it('permits member bob to do a transfer', async () => {
+      const { authClient } = await buildAuthClient(bobPrivateKey, {
+        host: getAuthHost(),
+        clientId
+      })
+
+      const response = await authClient.requestAccessToken(request)
+      expect(response).toMatchObject({ value: expect.any(String) })
+    })
+
+    it('permits member bob to do a second transfer', async () => {
+      const { authClient } = await buildAuthClient(bobPrivateKey, {
+        host: getAuthHost(),
+        clientId
+      })
+
+      const response = await authClient.requestAccessToken(request)
+      expect(response).toMatchObject({ value: expect.any(String) })
+    })
+
+    it('permits member bob to do a third transfer', async () => {
+      const { authClient } = await buildAuthClient(bobPrivateKey, {
+        host: getAuthHost(),
+        clientId
+      })
+
+      const response = await authClient.requestAccessToken(request)
+      expect(response).toMatchObject({ value: expect.any(String) })
+    })
+
+    it('forbids member bob to exceed the limit', async () => {
+      expect.assertions(1)
+      const { authClient } = await buildAuthClient(bobPrivateKey, {
+        host: getAuthHost(),
+        clientId
+      })
+
+      try {
+        await authClient.requestAccessToken(request)
+      } catch (error: any) {
+        expect(error.message).toEqual('Unauthorized')
+      }
+    })
+
+    it('permits admin alice to do a transfer', async () => {
+      const { authClient } = await buildAuthClient(alicePrivateKey, {
+        host: getAuthHost(),
+        clientId
+      })
+
+      const response = await authClient.requestAccessToken(request)
+      expect(response).toMatchObject({ value: expect.any(String) })
+    })
+  })
+
+  describe('approvals by groupId and spending limit', () => {
     const request: Request = {
       action: Action.SIGN_TRANSACTION,
       nonce: 'test-nonce',
