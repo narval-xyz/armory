db902a2656afa1e70718baa8e82cd73f2fe89d65
William Calderipe <wcalderipe@gmail.com>
2024-12-04 17:20:31 +0100

    Remove provider type draft

--- apps/vault/src/broker/core/type/provider.type.ts
+++
@@ -1,7 +0,0 @@
-// How to build a credential with the right private key Alg?
-//
-// Pass the input type that's part of the CreateConnection
-// Get a provider specific credential
-export interface CredentialBuilder<Input, Credential> {
-  build(input: Input): Promise<Credential>
-}
