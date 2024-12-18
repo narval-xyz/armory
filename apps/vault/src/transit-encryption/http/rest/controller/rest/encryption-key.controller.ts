7e7517c21cad07e9b488210d0bf298ff8f21a895
William Calderipe <wcalderipe@gmail.com>
2024-12-18 17:16:08 +0100

    Add CONNECTION_WRITE permission in the enc key (#46)

--- apps/vault/src/transit-encryption/http/rest/controller/rest/encryption-key.controller.ts
+++ apps/vault/src/transit-encryption/http/rest/controller/rest/encryption-key.controller.ts
@@ -5,6 +5,7 @@ import { Controller, HttpStatus, Post } from '@nestjs/common'
 import { ApiHeader, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { ClientId } from '../../../../../shared/decorator/client-id.decorator'
 import { PermissionGuard } from '../../../../../shared/decorator/permission-guard.decorator'
+import { VaultPermission } from '../../../../../shared/type/domain.type'
 import { EncryptionKeyService } from '../../../../core/service/encryption-key.service'
 import { EncryptionKeyDto } from '../dto/response/encryption-key.dto'
 
@@ -12,7 +13,7 @@ import { EncryptionKeyDto } from '../dto/response/encryption-key.dto'
   path: '/encryption-keys',
   version: '1'
 })
-@PermissionGuard(Permission.WALLET_IMPORT)
+@PermissionGuard(Permission.WALLET_IMPORT, VaultPermission.CONNECTION_WRITE)
 @ApiTags('Encryption Key')
 @ApiHeader({
   name: REQUEST_HEADER_CLIENT_ID,
